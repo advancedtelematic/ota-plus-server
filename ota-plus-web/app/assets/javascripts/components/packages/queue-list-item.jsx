e4819301927d632f08b36a07afa3a5c9e87ec6dd
Mateusz <mplacek@concisesoftware.com>
2016-05-19 14:04:07 +0200

    Fixed bugs on adminUI

--- ota-plus-web/app/assets/javascripts/components/packages/queue-list-item.jsx
+++ ota-plus-web/app/assets/javascripts/components/packages/queue-list-item.jsx
@@ -2,30 +2,18 @@ define(function(require) {
   var React = require('react'),
       ReactCSSTransitionGroup = React.addons.CSSTransitionGroup,
       Router = require('react-router'),
-      Link = Router.Link,
-      QueueListItemErrorlog = require('./queue-list-item-errorlog'),
-      QueueListItemLog = require('./queue-list-item-log');
+      Link = Router.Link;
 
   class QueueListItem extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        showLog: false
-      }
-      this.showLog = this.showLog.bind(this);
-    }
-    showLog() {
-      this.setState({
-        showLog: !this.state.showLog
-      });
     }
     render() {
       return (
-        <li className={'list-group-item ' + (this.state.showLog ? 'show-log ' : '') + this.props.status}>
+        <li className={'list-group-item ' + this.props.status}>
           <span className="list-group-item-text-left">{this.props.package.packageId.name}</span>
           <span className="drag-bar pull-right"><i className="fa fa-bars"></i></span>
           <button className="btn btn-action pull-right">{this.context.strings.cancel}</button>
-          <button className="btn btn-action pull-right" onClick={this.showLog}>{this.context.strings.log}</button>
           {this.props.status == 'error' ? 
             <button className="btn btn-action pull-right">retry</button>
           : null}
@@ -37,21 +25,6 @@ define(function(require) {
             : null}
             <strong>v. {this.props.package.packageId.version}</strong>
           </div>
-  
-          <ReactCSSTransitionGroup
-            transitionAppear={true}
-            transactionLeave={false}
-            transitionAppearTimeout={500}
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}
-            transitionName="example">
-            {this.state.showLog ? 
-              (this.props.status == 'error') ?
-                <QueueListItemErrorlog name={this.props.package.name}/>
-              :
-                <QueueListItemLog name={this.props.package.name}/>
-            : null}
-          </ReactCSSTransitionGroup>
         </li>
       );
     }
