dac42a76d3f248ea3e2b316439c55c6a8ca84de8
Calum McCall <calum@advancedtelematic.com>
2016-06-02 17:30:18 +0200

    import fixes

--- ota-plus-web/app/org/genivi/webserver/controllers/EventController.scala
+++ ota-plus-web/app/org/genivi/webserver/controllers/EventController.scala
@@ -1,30 +1,20 @@
 package org.genivi.webserver.controllers
 
-import javax.inject.{Inject, Named, Singleton}
+import javax.inject.Singleton
 
 import akka.actor.ActorRef
-import akka.stream.Materializer
 import akka.stream.scaladsl.Source
-import com.advancedtelematic.api.{ApiClientExec, ApiClientSupport}
 import com.advancedtelematic.ota.common.VehicleSeenMessage
-import com.advancedtelematic.ota.vehicle.Vehicles
 import org.genivi.sota.data.Vehicle
+import play.api.Logger
 import play.api.http.ContentTypes
 import play.api.libs.Comet
 import play.api.libs.concurrent.Execution
 import play.api.libs.json._
-import play.api.libs.ws._
 import play.api.mvc._
-import play.api.{Configuration, Logger}
 
 @Singleton
-class EventController @Inject()(val ws: WSClient,
-                                val conf: Configuration,
-                                materializer: Materializer,
-                                val clientExec: ApiClientExec,
-                                @Named("vehicles-store") vehiclesStore: Vehicles)
-extends Controller with ApiClientSupport
-  with OtaPlusConfig {
+class EventController extends Controller {
 
   implicit val context = Execution.defaultContext
 
@@ -38,7 +28,7 @@ extends Controller with ApiClientSupport
   val logger = Logger(this.getClass)
 
   def subVehicleSeen(vin: Vehicle.Vin): Action[AnyContent] = Action {
-    //TODO: is making a new actor here everytime best? Could we just have one declared in this controller?
+    //TODO: is making a new actor here every time best? Could we just have one declared in this controller?
     val vehicleSeenSource: Source[VehicleSeenMessage, ActorRef] = Source.actorPublisher(VehicleSeenEventActor.props)
     Ok.chunked(vehicleSeenSource
       .filter(m => m.vin == vin)
