7a9772405c128f75186f7a7ba08d0331834df529
Simão Mata <simao@advancedtelematic.com>
2016-06-28 16:19:38 +0200

    fix device registry deploy

--- ota-plus-device-registry/deploy/ci.sh
+++ ota-plus-device-registry/deploy/ci.sh
@@ -15,9 +15,9 @@ export IMAGE_ARTIFACT=${REGISTRY}/${IMAGE_NAME}:${DOCKER_TAG}
 export VAULT_ENDPOINT=$(echo $JOB_NAME | tr "-" "_")
 export VDATA=$(curl -s -H "X-Vault-Token: ${VAULT_TOKEN}" -X GET ${VAULT_ADDR}/v1/secret/$VAULT_ENDPOINT)
 
-export DEVICE_REGISTRY_DB_USER="$(echo $VDATA | jq -r .data.device_registry_db_username)"
-export DEVICE_REGISTRY_DB_PASSWORD="$(echo $VDATA | jq -r .data.device_registry_db_password)"
-export DEVICE_REGISTRY_DB_URL="$(echo $VDATA | jq -r .data.device_registry_db_url)"
+export DEVICE_REGISTRY_DB_USER="$(echo $VDATA | jq -r .data.db_username)"
+export DEVICE_REGISTRY_DB_PASSWORD="$(echo $VDATA | jq -r .data.db_password)"
+export DEVICE_REGISTRY_DB_URL="$(echo $VDATA | jq -r .data.db_url)"
 
 # export DEVICE_REGISTRY_AWS_ACCESS_KEY="$(echo $VDATA | jq -r .data.device_registry_aws_access_key)"
 # export DEVICE_REGISTRY_AWS_SECRET_KEY="$(echo $VDATA | jq -r .data.device_registry_aws_secret_key)"
