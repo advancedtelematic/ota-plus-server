38a27fe2d85c66149667fe4abb343c51cbcd362a
Simão Mata <simao@advancedtelematic.com>
2016-07-01 17:35:50 +0200

    Sign traceid with hmac
    Fix bugs in traceid, the id was not set on response.

--- ota-plus-web/app/com/advancedtelematic/TraceIdFilter.scala
+++ ota-plus-web/app/com/advancedtelematic/TraceIdFilter.scala
@@ -1,30 +1,41 @@
 package com.advancedtelematic
 
-import java.util.UUID
 import javax.inject.Inject
 
 import _root_.akka.stream.Materializer
 import com.advancedtelematic.ota.common.TraceId
 import play.api.mvc.{Filter, RequestHeader, Result}
-
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Success
+
 
 class TraceIdFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
 
   import TraceId._
 
-  private def validTraceId(requestHeader: RequestHeader): Option[UUID] =
-    parseValidHeader(requestHeader.headers.get(TRACEID_HEADER))
+  private def validTraceId(requestHeader: RequestHeader): Option[TraceId] = {
+    val idH = requestHeader.headers.get(TRACEID_HEADER)
+    val sigH = requestHeader.headers.get(TRACEID_HMAC_HEADER)
+    parseValidHeader(idH, sigH)
+  }
 
   override def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
-    val rh = if(validTraceId(requestHeader).isEmpty) {
-      val newHeaders = requestHeader.headers.remove(TRACEID_HEADER).add(TRACEID_HEADER -> newTraceId)
-      requestHeader.copy(headers = newHeaders)
+    val extracted = validTraceId(requestHeader)
+
+    val (rh, traceId) = if(extracted.isEmpty) {
+      val newId = newTraceId
+
+      val newHeaders = requestHeader.headers
+        .remove(TRACEID_HEADER)
+        .remove(TRACEID_HMAC_HEADER)
+        .add(TRACEID_HEADER -> newId.id, TRACEID_HMAC_HEADER -> newId.sig)
+
+      (requestHeader.copy(headers = newHeaders), newId)
     } else {
-      requestHeader
+      (requestHeader, extracted.get)
     }
 
-    nextFilter(rh)
+    nextFilter(rh) map { result =>
+      result.withHeaders(TRACEID_HEADER -> traceId.id, TRACEID_HMAC_HEADER -> traceId.sig)
+    }
   }
 }
