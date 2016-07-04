f12862da18cd4a7d6f2cf196b528d967e9f1fc49
Mateusz <mplacek@concisesoftware.com>
2016-07-04 15:29:15 +0200

    Added proper arrow. Removed unnecessary page

--- ota-plus-web/app/assets/javascripts/components/profile.jsx
+++ ota-plus-web/app/assets/javascripts/components/profile.jsx
@@ -1,66 +1,107 @@
 define(function(require) {
   var React = require('react'),
-      Router = require('react-router');
-	
-  var Profile = React.createClass({
-    render: function() {
+      Router = require('react-router'),
+      Link = Router.Link,
+      SotaDispatcher = require('sota-dispatcher');
+
+  class Profile extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        showEditField: false,
+        showEditButton: false,
+        usernameFieldLength: 0,
+        username: 'Username',
+      };
+      this.toggleEditField = this.toggleEditField.bind(this);
+      this.changeUsernameFieldLength = this.changeUsernameFieldLength.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    toggleEditField(e) {
+     if(e !== undefined)
+       e.preventDefault();
+     this.setState({
+        showEditField: !this.state.showEditField
+      });
+    }
+    toggleEditButton(state, e) {
+      e.preventDefault();
+      this.setState({
+        showEditButton: state
+      })
+    }
+    changeUsernameFieldLength() {
+      var val = this.refs.username.value;
+      this.setState({
+        usernameFieldLength: val.length
+      });
+    }
+    handleSubmit(e) {
+      e.preventDefault();
+      this.setState({
+        username: this.refs.username.value
+      });
+      this.toggleEditField();
+    }
+    render() {
       return (
-        <div>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="row">
-                <div className="col-md-12">
-                  <h3>{this.context.strings.profile}</h3>
-                </div>
-                <div className="col-md-6">
-                  <div className="thumbnail">
-                    <img src="http://cerebral-overload.com/wp-content/uploads/2016/02/ATS-AdvancedTelematicSystems-OTAPlus-RGB.jpg" alt="" />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <ul className="list-group">
-                    <li className="list-group-item">Your name: <strong>John Kowalski</strong></li>
-                    <li className="list-group-item">Your address: <strong>Kantstrasse 162 10623 Berlin, Germany</strong></li>
-                    <li className="list-group-item">Your phone: <strong>+4930959997540</strong></li>
-                    <li className="list-group-item">Your email address: <strong>ats.berlin@advancedtelematic.com</strong></li>
-                  </ul>
-                </div>
-              </div>
+        <div className="dropdown-menu dropdown-profile dropdown-right">
+          <form className="user-form" ref="userForm">
+            <div className="width-100 text-center pull-left">
+              <img src="/assets/img/icons/profile_icon_big.png" className="profile-icon" alt="" />
             </div>
-            <div className="col-md-6">
-              <div className="row">
-                <div className="col-md-12">
-                  <h3>{this.context.strings.editpassword}</h3>
-                </div>
-                <div className="col-md-6">
-                  <form ref='form'>
-                    <div className="form-group">
-                      <label>Old password</label>
-                      <input type="text" className="form-control" name="oldpassword" placeholder="Old password"/>
-                    </div>
-                    <div className="form-group">
-                      <label>New password</label>
-                      <input type="text" className="form-control" name="newpassword" placeholder="New password"/>
-                    </div>
-                    <div className="form-group">
-                      <label>Confirm new password</label>
-                      <input type="text" className="form-control" name="newpasswordconfirm" placeholder="Confirm new password"/>
-                    </div>
-                    <div className="form-group">
-                      <button type="submit" className="btn btn-primary">Save changes</button>
-                    </div>
-                  </form>  
-                </div>
+            <div className="width-100 text-center margin-top-10 pull-left">
+            {this.state.showEditField ?
+              <div>
+                <input className="input-username" name="username" type="text" placeholder={this.state.username} ref="username" onKeyUp={this.changeUsernameFieldLength}/>
+                
+                {this.state.usernameFieldLength > 0 ?
+                  <div className="pull-right">
+                    <a href="#" className="accept-button" onClick={this.handleSubmit}>
+                      <img src="/assets/img/icons/accept_icon.png" alt="" />
+                    </a>
+                    &nbsp;
+                    <a href="#" className="cancel-button" onClick={this.toggleEditField}>
+                      <img src="/assets/img/icons/close_icon.png" alt="" />
+                    </a>
+                  </div>
+                :
+                  <a href="#" className="pencil-button pencil-button-edit pull-right" onClick={this.toggleEditField}>
+                    <img src="/assets/img/icons/edit_icon.png" alt="" />
+                  </a>      
+                }
               </div>
+            : 
+              <div onMouseEnter={this.toggleEditButton.bind(this, true)} onMouseLeave={this.toggleEditButton.bind(this, false)}>
+                <strong><span className="username">{this.state.username}</span></strong>
+                {this.state.showEditButton ? 
+                  <a href="#" className="pencil-button pull-right" onClick={this.toggleEditField}>
+                    <img src="/assets/img/icons/edit_icon.png" alt="" />
+                  </a>
+                : null}
+              </div>
+            }
+            <div>
+              <span className="email">email@address.com</span>
+            </div>
+            </div>
+          </form>
+          <hr />
+          
+          <div className="profile-links">
+            <div>
+              <a href="/edit_profile">Edit profile</a>
+            </div>
+            <div>
+              <a href="#">Upgrade plan</a>
+            </div>
+            <div>
+              <a href="#" onClick={this.props.logout}>Sign out</a>
             </div>
           </div>
-        </div>
+        </div>  
       );
     }
-  });
-
-  Profile.contextTypes = {
-    strings: React.PropTypes.object.isRequired
   };
 
   return Profile;
