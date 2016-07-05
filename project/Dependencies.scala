8b74032374cdaabcaf270af0f0c97780d0e9e8bf
Stevan Andjelkovic <stevan@advancedtelematic.com>
2016-07-05 14:36:03 +0200

    Bump sota version.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Version {
   val Akka = "2.4.7"
   val JsonWebSecurity = "0.2.1"
   val MockWs = "2.5.0"
-  val GeniviSota = "0.1.75"
+  val GeniviSota = "0.1.79"
   val GeniviResolver = GeniviSota
   // Version 0.17 of akka-persistence-cassandra depends on Akka 2.4.7 and Scala 2.11.8
   // It is compatible with Cassandra 3.0.0 or higher
