4822e165111d028c17a3a92be8607dd1e2b1277a
Miguel Garcia <mgarcia@advancedtelematic.com>
2016-07-08 17:46:30 +0200

    PRO-840 bring test (ClientSdkControllerSpec) back to life

--- ota-plus-web/test/com/advancedtelematic/ota/ClientSdkControllerSpec.scala
+++ ota-plus-web/test/com/advancedtelematic/ota/ClientSdkControllerSpec.scala
@@ -1,10 +1,12 @@
 package com.advancedtelematic.ota
 
-import org.genivi.sota.data.{Vehicle, VinGenerators}
+import org.genivi.sota.data.Device.{DeviceId, DeviceName}
+import org.genivi.sota.data.{Device, Vehicle, VinGenerators}
 import org.scalatest.Tag
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
 import play.api.http.Status
+import play.api.libs.json.Json
 import play.api.libs.ws.{WSClient, WSRequest}
 
 /**
@@ -20,10 +22,11 @@ class ClientSdkControllerSpec extends PlaySpec
 
   import play.api.test.Helpers._
   import Generators._
+  import com.advancedtelematic.ota.device.Devices._
 
   override lazy val port = app.configuration.getString("test.webserver.port").map(_.toInt).getOrElse(9010)
 
-  "test download a preconfigured client" taggedAs APITests ignore { // TODO PRO-341
+  "test download a preconfigured client" taggedAs APITests in { // PRO-341 PRO-840
     import org.genivi.webserver.controllers.{Architecture, PackageType}
     val attempts = 5
     val wsClient = app.injector.instanceOf[WSClient]
@@ -34,11 +37,18 @@ class ClientSdkControllerSpec extends PlaySpec
           wsClient.url(s"http://localhost:$port/api/v1/$suffix")
         }
         // Step 1: Register Vin
-        val webappRegistrationLink = s"vehicles/${vin.get}"
-        val registrationResponse = await(fullUri(webappRegistrationLink).put(""))
-        registrationResponse.status mustBe Status.NO_CONTENT
+        val webappRegistrationLink = s"devices"
+        val uuid = java.util.UUID.randomUUID()
+        val device = org.genivi.sota.data.DeviceT(
+          deviceName = DeviceName("EXTRAPOWER2"),
+          deviceId = Some(DeviceId(uuid.toString)),
+          deviceType = Device.DeviceType.Other
+        )
+        val data = Json.toJson(device)
+        val registrationResponse = await(fullUri(webappRegistrationLink).put(data))
+        registrationResponse.status mustBe Status.CREATED
         // Step 2: Request preconf client
-        val webappDownloadLink = s"client/${vin.get}/${packfmt.fileExtension}/${arch.toString}"
+        val webappDownloadLink = s"client/${uuid.toString}/${packfmt.fileExtension}/${arch.toString}"
         val fileResponse = await(fullUri(webappDownloadLink).get)
         fileResponse.status mustBe Status.OK
     }
