ae8508f7468ad2a83bb101ea11128fe5aabca913
Calum McCall <calum@advancedtelematic.com>
2016-07-25 18:43:40 +0200

    Add device created event to web api
     - PRO-941

--- ota-plus-web/app/org/genivi/webserver/controllers/EventController.scala
+++ ota-plus-web/app/org/genivi/webserver/controllers/EventController.scala
@@ -5,17 +5,17 @@ import javax.inject.{Inject, Singleton}
 import akka.actor.ActorSystem
 import akka.stream.Materializer
 import akka.stream.scaladsl.Source
-import com.typesafe.config.ConfigException
-import org.genivi.sota.data.Device
+import com.advancedtelematic.ota.Messages.Messages.{deviceCreatedWrites, deviceSeenWrites}
+import org.genivi.sota.data.{Device, Namespace}
 import org.genivi.sota.messaging.MessageBusManager
-import org.genivi.sota.messaging.Messages.DeviceSeenMessage
+import org.genivi.sota.messaging.Messages.{DeviceCreated, DeviceSeen}
 import org.genivi.webserver.controllers.messaging.MessageBusConnection
-import play.api.{Configuration, Logger}
 import play.api.http.ContentTypes
 import play.api.libs.Comet
 import play.api.libs.concurrent.Execution
 import play.api.libs.json._
 import play.api.mvc._
+import play.api.Configuration
 
 @Singleton
 class EventController @Inject()
@@ -25,23 +25,23 @@ class EventController @Inject()
   extends Controller {
 
   implicit val context = Execution.defaultContext
-  private val log = Logger(this.getClass)
 
-  MessageBusManager.getSubscriber(system, conf.underlying)
-
-  implicit val deviceWrites = new Writes[DeviceSeenMessage] {
-   def writes(deviceMsg: DeviceSeenMessage) = Json.obj(
-     "deviceId" -> deviceMsg.deviceId.underlying.get,
-     "lastSeen" -> deviceMsg.lastSeen.toString
-   )
-  }
+  MessageBusManager.subscribeDeviceCreated(system, conf.underlying)
+  MessageBusManager.subscribeDeviceSeen(system, conf.underlying)
 
   def subDeviceSeen(device: Device.Id): Action[AnyContent] = Action {
-    val srcFn = subFn.getSource(system)
-    val deviceSeenSource: Source[DeviceSeenMessage, _] = srcFn(device)
+    val deviceSeenSource: Source[DeviceSeen, _] = subFn.getDeviceSeenSource(system)
     Ok.chunked(deviceSeenSource
+      .filter(dsm => dsm.deviceId == device)
       .map(Json.toJson(_))
-        via Comet.json("parent.deviceSeen")).as(ContentTypes.JSON)
+      via Comet.json("parent.deviceSeen")).as(ContentTypes.JSON)
   }
 
+  def subDeviceCreated(namespace: Namespace): Action[AnyContent] = Action {
+    val deviceCreatedSource: Source[DeviceCreated, _] = subFn.getDeviceCreatedSource(system)
+    Ok.chunked(deviceCreatedSource
+      .filter(dcm => dcm.namespace == namespace)
+      .map(Json.toJson(_))
+      via Comet.json("parent.deviceCreated")).as(ContentTypes.JSON)
+  }
 }
--- ota-plus-web/test/com/advancedtelematic/api/messaging/MessagingActorSpec.scala
+++ ota-plus-web/test/com/advancedtelematic/api/messaging/MessagingActorSpec.scala
@@ -6,16 +6,17 @@ import akka.stream.scaladsl.Source
 import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.TestKit
 import com.advancedtelematic.api.MessagingData
-import org.genivi.webserver.controllers.messaging.DeviceSeenActor
+import org.genivi.sota.messaging.Messages.DeviceSeen
+import org.genivi.webserver.controllers.messaging.Actors.MessageRelayActor
 import org.scalatest.WordSpecLike
 
 class MessagingActorSpec extends TestKit(ActorSystem()) with WordSpecLike {
 
   implicit val mat = ActorMaterializer()
 
-  "DeviceSeenActor" should {
-    "Push DeviceSeenMessages to akka stream" in {
-      val source = Source.actorPublisher(Props(new DeviceSeenActor(MessagingData.deviceId)))
+  "MessageRelayActor" should {
+    "push relevant messages to akka stream" in {
+      val source = Source.actorPublisher(Props(new MessageRelayActor[DeviceSeen]()))
       val stream = source.runWith(TestSink.probe)
       stream.request(1)
       system.eventStream.publish(MessagingData.deviceSeenMessage)
