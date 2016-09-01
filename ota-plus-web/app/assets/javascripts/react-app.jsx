63b7b842c82738b119d38a730fe5cb873565c4aa
Mateusz <mplacek@concisesoftware.com>
2016-09-01 19:24:13 +0200

    Fixed custom events handling in IE11

--- ota-plus-web/app/assets/javascripts/react-app.jsx
+++ ota-plus-web/app/assets/javascripts/react-app.jsx
@@ -1,3 +1,15 @@
 require(['es6!./app', 'jquery'], function(app, jQuery) {    
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+  }
+       
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent;
+  
   return app.run();
 });
