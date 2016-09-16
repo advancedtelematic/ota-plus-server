3623c708df99519f24921c42640e1cf52ee29a10
Mateusz <mplacek@concisesoftware.com>
2016-09-16 23:34:12 +0200

    Added package 'cancelled' status handling

--- ota-plus-web/app/assets/javascripts/components/packages/history-list-item-log.jsx
+++ ota-plus-web/app/assets/javascripts/components/packages/history-list-item-log.jsx
@@ -4,14 +4,20 @@ define(function(require) {
   class HistoryListItemLog extends React.Component {
     render() {
       return (
-        <div className="margin-top-10 pull-left">
-          <p><strong>{this.props.name}</strong> Log</p>
-          Package successfully installed on {this.props.completionTime.toDateString() + ' ' + this.props.completionTime.toLocaleTimeString()}
+        <div className="margin-top-10 width-100 pull-left">
+          {this.props.status == 'cancelled' ? 
+            <span>Installation</span>
+          :
+            <span>Package</span>
+          }
+          &nbsp; {this.props.status} on {this.props.time.toDateString() + ' ' + this.props.time.toLocaleTimeString()}
   
           <div className="log-code">
             {this.props.installationLog.resultText !== undefined && this.props.installationLog.resultText ? 
               this.props.installationLog.resultText
-            : <span>Log is empty</span>}
+            : 
+              <span>Log is empty</span>
+            }
           </div>
         </div>
       );
