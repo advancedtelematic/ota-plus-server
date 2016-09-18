3ec7134cbd696b877acd9cf2259367c28b0967b0
Mateusz <mplacek@concisesoftware.com>
2016-09-18 17:53:44 +0200

    Added queued package ID displaying

--- ota-plus-web/app/assets/javascripts/components/packages/queue-list-item.jsx
+++ ota-plus-web/app/assets/javascripts/components/packages/queue-list-item.jsx
@@ -20,31 +20,47 @@ define(function(require) {
       var packageName = this.props.package.packageId.name;
       return (
         <li className={'list-group-item ' + this.props.status + ' queue-item-status-' + this.props.package.status}>
-          <div className="queue-item-name pull-left">
-            {packageName}
-          </div>
-          {!_.isUndefined(this.props.package.status) && this.props.package.status == 'InFlight' ? 
-            <div className="queue-inflight-box pull-right">
-              in progress
+          <div className="width-100 pull-left">
+            <div className="queue-item-name pull-left">
+              {packageName}
             </div>
-          :
-            <span>
-              <span className="drag-bar pull-right"><i className="fa fa-bars"></i></span>
-              <button className="btn btn-action pull-right" onClick={this.cancelUpdate} id="button-cancel-update">{this.context.strings.cancel}</button>
-              {this.props.status == 'error' ?
-                <button className="btn btn-action pull-right">retry</button>
-              : null}
-            </span>
-          }
-          
-          <div className="pull-right list-group-item-text-right">
-            {this.props.status == 'error' ?
-              <span className="fa-stack package-status-icon">
-                <i className="fa fa-times-circle fa-stack-1x red" aria-hidden="true"></i>
+            {!_.isUndefined(this.props.package.status) && this.props.package.status == 'InFlight' ? 
+              <div className="queue-inflight-box pull-right">
+                in progress
+              </div>
+            :
+              <span>
+                <span className="drag-bar pull-right"><i className="fa fa-bars"></i></span>
+                <button className="btn btn-action pull-right" onClick={this.cancelUpdate} id="button-cancel-update">{this.context.strings.cancel}</button>
+                {this.props.status == 'error' ?
+                  <button className="btn btn-action pull-right">retry</button>
+                : null}
               </span>
-            : null}
-            <strong>v. {this.props.package.packageId.version}</strong>
+            }
+          
+            <div className="pull-right list-group-item-text-right">
+              {this.props.status == 'error' ?
+                <span className="fa-stack package-status-icon">
+                  <i className="fa fa-times-circle fa-stack-1x red" aria-hidden="true"></i>
+                </span>
+              : null}
+              <strong>v. {this.props.package.packageId.version}</strong>
+            </div>
           </div>
+          
+          {!_.isUndefined(this.props.package.status) && this.props.package.status == 'InFlight' ? 
+            <div className="width-100 pull-left lightgrey font-10">
+              <div>
+                Queued on: {new Date(this.props.package.createdAt).toDateString()} {new Date(this.props.package.createdAt).toLocaleTimeString()}
+              </div>
+              <div className="margin-top-5">
+                Installation started on: {new Date(this.props.package.updatedAt).toDateString()} {new Date(this.props.package.updatedAt).toLocaleTimeString()}
+              </div>
+              <div className="margin-top-5">
+                Update identifier: {this.props.package.requestId}
+              </div>
+            </div>
+          : undefined}
         </li>
       );
     }
