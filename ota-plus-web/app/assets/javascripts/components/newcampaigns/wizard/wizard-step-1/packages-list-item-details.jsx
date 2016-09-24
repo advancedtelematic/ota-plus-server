1914ecec84f822d7b8189f17980430fa0b469156
Mateusz <mplacek@concisesoftware.com>
2016-09-25 01:31:49 +0200

    Campaign creation/wizard

---
+++ ota-plus-web/app/assets/javascripts/components/newcampaigns/wizard/wizard-step-1/packages-list-item-details.jsx
@@ -0,0 +1,33 @@
+define(function(require) {
+  var React = require('react'),
+      SotaDispatcher = require('sota-dispatcher'),
+      PackagesListItemDetailsVersion = require('es6!./packages-list-item-details-version');
+
+  class PackageListItemDetails extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      var versions = _.map(this.props.versions, function(version, i) {
+        var isChosen = this.props.chosenPackage.name == version.id.name && this.props.chosenPackage.version == version.id.version;
+        return (
+          <PackagesListItemDetailsVersion
+            version={version}
+            key={'package-' + this.props.packageName + '-' + version.id.version}
+            choosePackage={this.props.choosePackage}
+            isChosen={isChosen}/>
+        );
+      }, this);
+
+      return (
+        <div className="package-details">
+          <ul>
+            {versions}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return PackageListItemDetails;
+});
