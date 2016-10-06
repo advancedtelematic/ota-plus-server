66dd237ce36dd624aac23d4915289237fae9ce82
Simão Mata <simao@advancedtelematic.com>
2016-10-06 14:31:46 +0200

    use latest common-messaging

--- ota-plus-web/deploy/ci.sh
+++ ota-plus-web/deploy/ci.sh
@@ -7,7 +7,8 @@ export IMAGE_NAME="ota-plus-web"
 export REGISTRY="advancedtelematic"
 export DOCKER_TAG=`cat ../deploy/version.properties`
 export IMAGE_ARTIFACT=${REGISTRY}/${IMAGE_NAME}:${DOCKER_TAG}
-export KINESIS_STREAM_NAME_SUFFIX=${DEPLOY_ENV-production}
+export KAFKA_TOPIC_SUFFIX=${DEPLOY_ENV-production}
+export MESSAGING_MODE=kafka
 
 # Merge service environment variables with secrets from this vault endpoint.
 export CATALOG_ADDR="http://catalog.gw.prod01.internal.advancedtelematic.com"
