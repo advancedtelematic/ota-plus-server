53f298dbba9733df128f11a2db4169de3d8ca7d7
Simão Mata <simao@advancedtelematic.com>
2016-10-06 14:31:46 +0200

    use latest common-messaging

--- ota-plus-web/app/com/advancedtelematic/ota/Messages/WebMessageBusListenerActor.scala
+++ ota-plus-web/app/com/advancedtelematic/ota/Messages/WebMessageBusListenerActor.scala
@@ -1,36 +1,29 @@
 package com.advancedtelematic.ota.Messages
 
-import akka.actor.{ActorLogging, PoisonPill, Props}
-import akka.stream.actor.ActorSubscriberMessage.{OnComplete, OnError, OnNext}
-import akka.stream.actor.{ActorSubscriber, RequestStrategy, WatermarkRequestStrategy}
-import org.genivi.sota.messaging.Messages.{Message, MessageLike}
+import akka.actor.{ActorSystem, Props}
+import cats.data.Xor
+import org.genivi.sota.messaging.MessageBus
+import org.genivi.sota.messaging.Messages.{BusMessage, MessageLike}
 import org.genivi.sota.messaging.daemon.MessageBusListenerActor
+import org.slf4j.LoggerFactory
+
 import scala.reflect.runtime.universe._
 
-class WebMessageBusListenerActor[T <: Message]()(implicit ml: MessageLike[T], tt: TypeTag[T])
-  extends ActorSubscriber with ActorLogging {
-
-  override protected def requestStrategy: RequestStrategy = WatermarkRequestStrategy(1024)
-
-  implicit val ec = context.dispatcher
-
-  override def receive: Receive = {
-    case OnNext(e) if ml.tag.runtimeClass.isInstance(e) =>
-      val msg = e.asInstanceOf[T]
-      log.info(s"Got message ${ml.streamName} - ${ml.id(msg)} publishing to Akka EventStream")
-      context.system.eventStream.publish(msg)
-
-    case OnComplete =>
-      log.info(s"${context.self.path.name} Upstream completed")
-      self ! PoisonPill
-
-    case OnError(ex) =>
-      log.info(s"Error from upstream: ${ex.getMessage}")
-      throw ex
-  }
-}
-
 object WebMessageBusListenerActor {
-  def props[T <: Message](implicit ml: MessageLike[T], tt: TypeTag[T]): Props =
-    MessageBusListenerActor.props[T](Props(new WebMessageBusListenerActor[T]()))
+  val _log = LoggerFactory.getLogger(this.getClass)
+
+  def props[M <: BusMessage]()(implicit ml: MessageLike[M], tt: TypeTag[M], system: ActorSystem): Props = {
+    val source = MessageBus.subscribe(system, system.settings.config) match {
+      case Xor.Right(s) =>
+        s.map { msg =>
+          _log.info(s"Received ${ml.streamName} -> ${ml.id(msg)}, publishing to akka event stream")
+          system.eventStream.publish(msg)
+          msg
+        }
+
+      case Xor.Left(err) => throw err
+    }
+
+    MessageBusListenerActor.props(source)
+  }
 }
