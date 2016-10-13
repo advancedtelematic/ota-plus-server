c0f65fa0fb836c863041bb995e847ba16e2c10b4
Mateusz <mplacek@concisesoftware.com>
2016-10-13 16:19:11 +0200

    Added packages and groups information restoring after save on campaign wizard

--- ota-plus-web/app/assets/javascripts/components/newcampaigns/wizard/wizard-step-1.jsx
+++ ota-plus-web/app/assets/javascripts/components/newcampaigns/wizard/wizard-step-1.jsx
@@ -11,7 +11,7 @@ define(function(require) {
     setWizardData(packageName, packageVersion) {
       SotaDispatcher.dispatch({
         actionType: 'set-package-for-campaign',
-        uuid: this.props.campaign.id,
+        uuid: this.props.campaign.meta.id,
         data: {name: packageName, version: packageVersion}
       });
       this.props.setWizardData({packageName: packageName, packageVersion: packageVersion});
