1d7e2b86b660f2b5b67f6d5fe9b8502fafe1e6db
Mateusz <mplacek@concisesoftware.com>
2016-10-23 11:09:52 +0200

    Added real names on detailed view when campaign is launched

--- ota-plus-web/app/assets/javascripts/components/newcampaigns/campaign-groups-list.jsx
+++ ota-plus-web/app/assets/javascripts/components/newcampaigns/campaign-groups-list.jsx
@@ -1,37 +1,54 @@
 define(function(require) {
   var React = require('react'),
+      SotaDispatcher = require('sota-dispatcher'),
       db = require('stores/db'),
-      CampaignGroupsListItem = require('es6!./campaign-groups-list-item');
+      CampaignGroupsListItem = require('es6!./campaign-groups-list-item'),
+      Loader = require('es6!../loader');
       
   class CampaignGroupsList extends React.Component {
     constructor(props) {
       super(props);
+      SotaDispatcher.dispatch({actionType: 'get-groups'});
+      db.groups.addWatch("poll-campaign-details-groups", _.bind(this.forceUpdate, this, null));
+    }
+    componentWillUnmount() {
+      db.groups.addWatch("poll-campaign-details-groups");
     }
     render() {
-      var groups = _.map(this.props.groups, function(group) {
-        return (
-          <CampaignGroupsListItem 
-            key={"campaign-group-" + group.updateRequest}
-            campaignGroup={group} 
-            cancelCampaignForGroup={this.props.cancelCampaignForGroup}/>
-        );
-      }, this);
+      var groupsData = db.groups.deref();
+      if(!_.isUndefined(groupsData)) {
+        var groups = _.map(this.props.groups, function(group) {
+          var foundGroup = _.findWhere(groupsData, {id: group.group});
+          return (
+            <CampaignGroupsListItem 
+              key={"campaign-group-" + group.updateRequest}
+              groupName={foundGroup.groupName}
+              campaignGroup={group} 
+              cancelCampaignForGroup={this.props.cancelCampaignForGroup}/>
+          );
+        }, this);
+      }
         
       return (
         <div>
-          <table className="table table-grey-header">
-            <thead>
-              <tr>
-                <th className="col-md-4">Name</th>
-                <th className="col-md-4 text-center">Status</th>
-                <th className="col-md-3"></th>
-                <th className="col-md-1"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {groups}
-            </tbody>
-          </table>
+          {!_.isUndefined(groupsData) ? 
+            <table className="table table-grey-header">
+              <thead>
+                <tr>
+                  <th className="col-md-4">Name</th>
+                  <th className="col-md-4 text-center">Status</th>
+                  <th className="col-md-3"></th>
+                  <th className="col-md-1"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {groups}
+              </tbody>
+            </table>
+          : undefined}
+          {_.isUndefined(groupsData) ? 
+            <Loader />
+          : undefined}
         </div>
       );
     }
