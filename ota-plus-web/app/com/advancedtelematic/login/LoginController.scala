3d8fc8ce5a6d8ec12943b9ec3cab4b082344cefb
Jerry Trieu <jerry@advancedtelematic.com>
2016-11-02 16:03:02 +0100

    Revert "Sanitize authorizationError.scala.html"
    This reverts commit a4c034b3afabdfaecfeb76bc7092c7b457c8335c.

--- ota-plus-web/app/com/advancedtelematic/login/LoginController.scala
+++ ota-plus-web/app/com/advancedtelematic/login/LoginController.scala
@@ -82,7 +82,7 @@ class LoginController @Inject()(conf: Configuration, val messagesApi: MessagesAp
   }
 
   def authorizationFailed(error: String, errorDescription: String): Result = {
-    Redirect(routes.LoginController.authorizationError()).flashing("authzError" -> error)
+    Redirect(routes.LoginController.authorizationError()).flashing("authzError" -> errorDescription)
   }
 
   private[this] def exchangeIdTokenForAssertion(idToken: IdToken): AsyncDescribedComputation[JwtAssertion] = {
