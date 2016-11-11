2b8116e07dc1c4f117f870263cc892b8dc03ae32
Mateusz <mplacek@concisesoftware.com>
2016-11-11 17:49:44 +0100

    Added adding device to group using d&d, implemented devices counting in a group

--- ota-plus-web/app/assets/javascripts/components/groups/groups-artificial.jsx
+++ ota-plus-web/app/assets/javascripts/components/groups/groups-artificial.jsx
@@ -5,29 +5,71 @@ define(function(require) {
   class GroupsArtificial extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        artificialGroupsData: {
+          all: {
+            groupName: 'all',
+            devicesCount: null,
+            friendlyName: 'All devices'
+          },
+          ungrouped: {
+            groupName: 'ungrouped',
+            devicesCount: null,
+            friendlyName: 'Ungrouped devices'
+          }
+        }
+      };
+      this.prepareGroups = this.prepareGroups.bind(this);
+    }
+    componentDidMount() {
+      this.prepareGroups(this.props.groups, this.props.devices);
+    }
+    componentWillReceiveProps(nextProps) {
+      if(!_.isEqual(this.props.devices, nextProps.devices) || !_.isEqual(this.props.groups, nextProps.groups)) {
+        this.prepareGroups(nextProps.groups, nextProps.devices);
+      }
+    }
+    prepareGroups(groups, devices) {
+      var ungroupedDevicesCount;
+      var artificialGroupsData = this.state.artificialGroupsData;
+      artificialGroupsData.all.devicesCount = Object.keys(devices).length;
+      
+      var discardedDeviceUUIDs = [];
+      _.each(groups, function(group) {
+        _.each(group.devicesUUIDs, function(uuid) {
+          if(discardedDeviceUUIDs.indexOf(uuid) < 0) {
+            devices = _.reject(devices, function(device) {
+              return device.uuid == uuid;
+            });
+            discardedDeviceUUIDs.push(uuid);
+          }
+        });
+      });
+      
+      artificialGroupsData.ungrouped.devicesCount = Object.keys(devices).length;
+      
+      this.setState({
+        artificialGroupsData: artificialGroupsData
+      });
     }
     render() {
       var selectedArtificialGroupName = (this.props.selectedGroup.type == 'artificial' ? this.props.selectedGroup.name : null);
-        
+      var groups = _.map(this.state.artificialGroupsData, function(group, index) {
+        return (
+          <button key={"button-artificial-group-" + group.groupName} type="button" className={"list-group-item " + (selectedArtificialGroupName == group.groupName ? " checked" : "")} onClick={this.props.selectGroup.bind(this, {name: group.groupName, type: 'artificial'})} id={"button-artificial-group-" + group.groupName}>
+            <div className="pull-left">
+              <div className="group-text">
+                <div className="group-title" title={group.friendlyName}>{group.friendlyName}</div>
+                <div className="group-subtitle">{group.devicesCount} devices</div>
+              </div>
+            </div>
+          </button>
+        );
+      }, this);
+      
       return (
         <div>
-          <button type="button" className={"list-group-item" + (selectedArtificialGroupName == 'all' ? " checked" : "")} onClick={this.props.selectGroup.bind(this, {name: 'all', type: 'artificial'})} id="button-group-all-devices">
-            <div className="pull-left">
-              <div className="group-text">
-                <div className="group-title" title="All devices">All devices</div>
-                <div className="group-subtitle">X devices</div>
-              </div>
-            </div>
-          </button>
-              
-          <button type="button" className={"list-group-item" + (selectedArtificialGroupName == 'ungrouped' ? " checked" : "")} onClick={this.props.selectGroup.bind(this, {name: 'ungrouped', type: 'artificial'})} id="button-group-ungrouped-devices">
-            <div className="pull-left">
-              <div className="group-text">
-                <div className="group-title" title="Ungrouped devices">Ungrouped devices</div>
-                <div className="group-subtitle">Y devices</div>
-              </div>
-            </div>
-          </button>
+          {groups}
         </div>
       );
     }
--- ota-plus-web/app/assets/javascripts/components/groups/groups-list.jsx
+++ ota-plus-web/app/assets/javascripts/components/groups/groups-list.jsx
@@ -12,16 +12,95 @@ define(function(require) {
   class GroupsList extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        groupsData: undefined,
+        overGroupUUID: null
+      };
+      this.prepareGroups = this.prepareGroups.bind(this);
+      this.onDragOver = this.onDragOver.bind(this);
+      this.onDragLeave = this.onDragLeave.bind(this);
+      this.onDrop = this.onDrop.bind(this);
+      this.handleResponse = this.handleResponse.bind(this);
+      db.postStatus.addWatch("poll-add-device-to-group", _.bind(this.handleResponse, this, null));
     }
-    render() {
-      var groups = _.map(this.props.groups, function(group, index) {
-        if(!_.isUndefined(group)) {
+    componentDidMount() {
+      this.prepareGroups(this.props.groups, this.props.devices);
+    }
+    componentWillReceiveProps(nextProps) {
+      if(!nextProps.draggingDeviceUUID && this.props.draggingDeviceUUID !== nextProps.draggingDeviceUUID) {
+        this.setState({overGroupUUID: null});
+      }
+      if(!_.isEqual(this.props.devices, nextProps.devices) || !_.isEqual(this.props.groups, nextProps.groups)) {
+        this.prepareGroups(nextProps.groups, nextProps.devices);
+      }
+    }
+    componentWillUnmount() {
+      db.postStatus.removeWatch("poll-add-device-to-group");
+    }
+    prepareGroups(groupsData, devicesData) {
+      var devicesUUIDs = _.pluck(devicesData, 'uuid');
+      var newGroups = _.clone(groupsData);
+      _.each(newGroups, function(group, index) {
+        group.devicesFilteredUUIDs = _.filter(group.devicesUUIDs, function(device) {
+          return (devicesUUIDs.indexOf(device) > -1);
+        });
+      });
+      this.setState({groupsData: newGroups});
+    }
+    onDragOver(e) {
+      e.preventDefault();
+      this.setState({overGroupUUID: e.currentTarget.dataset.groupuuid});
+    }
+    onDragLeave(e) {
+      this.setState({overGroupUUID: null});
+    }
+    onDrop(e) {
+      if(e.preventDefault)
+        e.preventDefault();
+      var draggingUUID = this.props.draggingDeviceUUID;
+      var dropUUID = e.currentTarget.dataset.groupuuid;
+      
+      if(draggingUUID !== null && dropUUID !== null) {
+        SotaDispatcher.dispatch({
+          actionType: 'add-device-to-group',
+          uuid: dropUUID,
+          deviceId: draggingUUID
+        });
+      }
+    }
+    handleResponse() {
+      var postStatus = !_.isUndefined(db.postStatus.deref()) ? db.postStatus.deref() : undefined;
+      if(!_.isUndefined(postStatus['add-device-to-group'])) {
+        if(postStatus['add-device-to-group'].status === 'success') {
+          delete postStatus['add-device-to-group'];
+          db.postStatus.reset(postStatus);
+          setTimeout(function() {
+            SotaDispatcher.dispatch({actionType: 'get-groups'});
+          }, 1);
+        }
+      }
+    }
+    render() {        
+      var className = '';
+      if(this.props.draggingDeviceUUID !== null) {
+        className = 'droppable';
+      }
+        
+      var groups = _.map(this.state.groupsData, function(group, index) {
+        if(!_.isUndefined(group) && Object.keys(group.devicesFilteredUUIDs).length) {
+          var groupClassName = (this.state.overGroupUUID !== null && group.id == this.state.overGroupUUID) ? className + " active" : className;
           return (
-            <li key={'group-' + group.groupName}>
+            <li 
+              key={'group-' + group.groupName}
+              onDragOver={this.onDragOver}
+              onDragLeave={this.onDragLeave}
+              onDrop={this.onDrop}
+              data-groupuuid={group.id}>
               <GroupsListItem 
                 group={group}
                 selectGroup={this.props.selectGroup}
-                isSelected={(this.props.selectedGroup.name == group.groupName && this.props.selectedGroup.type == 'real')}/>
+                isSelected={(this.props.selectedGroup.name == group.groupName && this.props.selectedGroup.type == 'real')}
+                groupClassName={groupClassName}/>
             </li>
           );
         }
