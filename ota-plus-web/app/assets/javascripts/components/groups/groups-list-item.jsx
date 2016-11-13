8e0f564ae3e340b5571359490330c67106233f19
Mateusz <mplacek@concisesoftware.com>
2016-11-13 04:21:35 +0100

    Added action buttons for groups

--- ota-plus-web/app/assets/javascripts/components/groups/groups-list-item.jsx
+++ ota-plus-web/app/assets/javascripts/components/groups/groups-list-item.jsx
@@ -5,10 +5,21 @@ define(function(require) {
   class GroupsListItem extends React.Component {
     constructor(props) {
       super(props);
+      this.selectGroup = this.selectGroup.bind(this);
+      this.renameGroup = this.renameGroup.bind(this);
+    }
+    selectGroup(e) {
+      e.preventDefault();
+      if(e.target.className.indexOf('fa') === -1 && e.target.tagName.toLowerCase() !== 'li' && e.target.tagName.toLowerCase() !== 'span')
+        this.props.selectGroup({name: this.props.group.groupName, type: 'real', uuid: this.props.group.id});
+    }
+    renameGroup(e) {
+      e.preventDefault();
+      this.props.openRenameGroupModal(this.props.group);
     }
     render() {
       return (
-        <button type="button" className={"list-group-item " + this.props.groupClassName + (this.props.isSelected ? " checked" : "")} onClick={this.props.selectGroup.bind(this, {name: this.props.group.groupName, type: 'real', uuid: this.props.group.id})} id={"button-group-" + this.props.group.groupName}>
+        <button type="button" className={"list-group-item " + this.props.groupClassName + (this.props.isSelected ? " checked" : "")} onClick={this.selectGroup} id={"button-group-" + this.props.group.groupName}>
           <div className="group-icon"></div>
           <div className="group-text">
             <div className="group-title" title={this.props.group.groupName}>{this.props.group.groupName}</div>
@@ -19,6 +30,16 @@ define(function(require) {
               <i className="fa fa-angle-right fa-3x"></i>
             </div>
           : null}
+          <div className="dropdown action-menu-dropdown pull-right">
+            <div data-toggle="dropdown">
+              <i className="fa fa-chevron-down" aria-hidden="true"></i>
+            </div>
+            <ul className="dropdown-menu">
+              <li onClick={this.renameGroup}>
+                <img src="/assets/img/icons/edit_black.png" alt="" style={{width: '15px'}}/> Rename
+              </li>
+            </ul>
+          </div>
         </button>
       );
     }
