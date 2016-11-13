1dc62c2396b9b0a08ce07feefe22b687ba0225e9
Mateusz <mplacek@concisesoftware.com>
2016-11-13 04:21:35 +0100

    Fixed groups name truncating

--- ota-plus-web/app/assets/javascripts/components/groups/groups-list-item.jsx
+++ ota-plus-web/app/assets/javascripts/components/groups/groups-list-item.jsx
@@ -9,12 +9,10 @@ define(function(require) {
     render() {
       return (
         <button type="button" className={"list-group-item " + this.props.groupClassName + (this.props.isSelected ? " checked" : "")} onClick={this.props.selectGroup.bind(this, {name: this.props.group.groupName, type: 'real', uuid: this.props.group.id})} id={"button-group-" + this.props.group.groupName}>
-          <div className="pull-left">
-            <div className="group-icon"></div>
-            <div className="group-text">
-              <div className="group-title" title={this.props.group.groupName}>{this.props.group.groupName}</div>
-              <div className="group-subtitle">{Object.keys(this.props.group.devicesFilteredUUIDs).length} devices</div>
-            </div>
+          <div className="group-icon"></div>
+          <div className="group-text">
+            <div className="group-title" title={this.props.group.groupName}>{this.props.group.groupName}</div>
+            <div className="group-subtitle">{Object.keys(this.props.group.devicesFilteredUUIDs).length} devices</div>
           </div>
           {this.props.isSelected ? 
             <div className="group-pointer">
