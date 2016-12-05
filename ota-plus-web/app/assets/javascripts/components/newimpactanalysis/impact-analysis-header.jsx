a54e0f2f65b2238b27bd65140d996993993e762c
Mateusz <mplacek@concisesoftware.com>
2016-12-05 16:20:45 +0100

    Small fixes on new impact analysis page

--- ota-plus-web/app/assets/javascripts/components/newimpactanalysis/impact-analysis-header.jsx
+++ ota-plus-web/app/assets/javascripts/components/newimpactanalysis/impact-analysis-header.jsx
@@ -1,8 +1,5 @@
 define(function(require) {
-  var React = require('react'),
-      Router = require('react-router'),
-      Link = Router.Link,
-      db = require('stores/db');
+  var React = require('react');
       
   class ImpactAnalysisHeader extends React.Component {
     constructor(props) {
@@ -13,7 +10,6 @@ define(function(require) {
       return (
         <div className="grey-header">      
           <div className="col-md-12">
-            <Link to="/"><img src="/assets/img/icons/back.png" className="icon-back" alt=""/></Link>
             <div className="grey-header-icon"></div>
             <div className="grey-header-text">
               <div className="grey-header-title">Impact analyser</div>
