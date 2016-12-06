50483e1a714fd650f277189af5045c3437e6ab16
Mateusz <mplacek@concisesoftware.com>
2016-12-06 03:32:12 +0100

    Truncated version on queued/history update requests

--- ota-plus-web/app/assets/javascripts/components/packages/queue-list-item.jsx
+++ ota-plus-web/app/assets/javascripts/components/packages/queue-list-item.jsx
@@ -45,7 +45,9 @@ define(function(require) {
                   <i className="fa fa-times-circle fa-stack-1x red" aria-hidden="true"></i>
                 </span>
               : null}
-              <strong>v. {this.props.package.packageId.version}</strong>
+              <div className="queue-item-version pull-right" title={this.props.package.packageId.version}>
+                <strong>v. {this.props.package.packageId.version}</strong>
+              </div>
             </div>
           </div>
           
