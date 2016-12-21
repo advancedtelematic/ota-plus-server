e0c187e0d81ecfa8d067bc58c457af48117041d6
Mateusz <mplacek@concisesoftware.com>
2016-12-21 20:24:29 +0100

    Implemented packages on device vs in garage in device detailed view

--- ota-plus-web/app/assets/javascripts/components/packages/packages-list-item.jsx
+++ ota-plus-web/app/assets/javascripts/components/packages/packages-list-item.jsx
@@ -1,31 +1,20 @@
 define(function(require) {
-  var React = require('react'),
-      ReactDOM = require('react-dom'),
-      db = require('stores/db');
+  var React = require('react');
 
   class PackagesListItem extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        isAutoInstallEnabled: this.props.isAutoInstallEnabled,
-      };
       this.itemClick = this.itemClick.bind(this);
-    }
-    componentWillReceiveProps(nextProps) {
-      if(nextProps.isAutoInstallEnabled !== this.props.isAutoInstallEnabled) {
-        this.setState({isAutoInstallEnabled: nextProps.isAutoInstallEnabled});
-      }
     }
     itemClick(e) {
       this.props.expandPackage(this.props.name);
     }
     render() {
-      var packageName = this.props.name;
       return (
         <div className="list-group-item-wrapper">
           <button type="button" className="list-group-item" onClick={this.itemClick} id={"button-package-" + this.props.name}>
             <div className="package-item-name pull-left">
-              <span title={packageName}>{packageName}</span>
+              <span title={this.props.name}>{this.props.name}</span>
             </div>
           
             <div className="package-item-status pull-right">
@@ -33,26 +22,16 @@ define(function(require) {
                 {!this.props.isSelected ?
                   this.props.installedPackage ?
                     <span>
-                      <span className="pull-right">installed</span>
-                      <span className="package-name pull-right" title={this.props.installedPackage}>{this.props.installedPackage}&nbsp;</span>
-                      <span className="pull-right">v.&nbsp;</span>
-                      {this.props.isBlackListed ? 
-                        <i className="fa fa-exclamation-triangle icon-exclamation pull-right"></i>
-                      :
-                        <span className="fa-stack package-status-circle pull-right">
-                          <i className="fa fa-circle fa-stack-1x"></i>
-                          <i className="fa fa-check-circle fa-stack-1x green" aria-hidden="true"></i>
-                        </span>
-                      }
-                      {this.props.isAutoInstallEnabled ?
-                        <span className="pull-right">
-                          <label className="label label-success label-auto-update">Auto</label>
-                        </span>
-                      : null}
+                      <span className="pull-right">
+                        <label className={"label label-auto-update" + (this.props.isAutoInstallEnabled ? " active" : "")}>Auto</label>
+                      </span>
                     </span>
                   : 
                     this.props.queuedPackage ?
                       <span>
+                        <span className="pull-right">
+                          <label className={"label label-auto-update" + (this.props.isAutoInstallEnabled ? " active" : "")}>Auto</label>
+                        </span>
                         <span className="pull-right">queued</span>
                         <span className="package-name pull-right" title={this.props.queuedPackage}>{this.props.queuedPackage}&nbsp;</span>
                         <span className="pull-right">v.&nbsp;</span>
@@ -60,20 +39,12 @@ define(function(require) {
                           <i className="fa fa-circle fa-stack-1x"></i>
                           <i className="fa fa-dot-circle-o fa-stack-1x orange" aria-hidden="true"></i>
                         </span>
-                        {this.props.isAutoInstallEnabled ? 
-                          <span className="pull-right">
-                            <label className="label label-success label-auto-update">Auto</label>
-                          </span>
-                        : null}
                       </span>
                     : 
                       <span>
-                        <span className="package-status-label-uninstalled pull-right">Uninstalled</span>
-                        {this.props.isAutoInstallEnabled ? 
-                          <span className="pull-right">
-                            <label className="label label-success label-auto-update">Auto</label>
-                          </span>
-                        : null}
+                        <span className="pull-right">
+                          <label className={"label label-auto-update" + (this.props.isAutoInstallEnabled ? " active" : "")}>Auto</label>
+                        </span>
                       </span>
                 : null}
               </div>
@@ -82,16 +53,20 @@ define(function(require) {
           {this.props.isSelected ?
             <div className="auto-update-toggle">
               Automatic update
-              <div className={"switch" + (this.state.isAutoInstallEnabled ? " switchOn" : "")} onClick={this.props.toggleAutoInstall.bind(this, this.props.name, this.state.isAutoInstallEnabled)}></div>
+              <div className={"switch" + (this.props.isAutoInstallEnabled ? " switchOn" : "")} onClick={this.props.toggleAutoInstall.bind(this, this.props.name, this.props.isAutoInstallEnabled)}>
+                <div className="switch-status">
+                  {this.props.isAutoInstallEnabled ?
+                    <span>ON</span>
+                  :
+                    <span>OFF</span>
+                  }
+                </div>
+              </div>
             </div>
           : null}
         </div>
       );
     }
-  };
-
-  PackagesListItem.contextTypes = {
-    location: React.PropTypes.object,
   };
 
   return PackagesListItem;
