fd3ed0b2e456de2589e6a1fbe34d0715ac5a841e
Mateusz <mplacek@concisesoftware.com>
2017-01-11 11:06:02 +0100

    Fixed empty list after package creating

--- ota-plus-web/app/assets/javascripts/components/packages-page/packages.jsx
+++ ota-plus-web/app/assets/javascripts/components/packages-page/packages.jsx
@@ -106,7 +106,7 @@ define(function(require) {
       var searchablePackages = db.searchablePackages.deref();
       if(!_.isUndefined(searchablePackages)) {
         if(_.isUndefined(this.state.searchablePackagesData))
-          this.setState({areInitPackagesFound: Object.keys(searchablePackages).length > 0});
+          this.setState({c: Object.keys(searchablePackages).length > 0});
       
         searchablePackages = this.groupAndSortPackages(searchablePackages, selectedSort);
         if(_.isUndefined(this.state.searchablePackagesData) || Object.keys(this.state.searchablePackagesData)[0] !== Object.keys(searchablePackages)[0])
@@ -224,6 +224,7 @@ define(function(require) {
         if(_.isUndefined(this.state.searchablePackagesData) || Object.keys(this.state.searchablePackagesData)[0] !== Object.keys(searchablePackages)[0])
           this.setFakeHeader(Object.keys(searchablePackages)[0]);
         this.setState({
+          areInitPackagesFound: true,
           searchablePackagesData: searchablePackages,
           searchablePackagesDataNotChanged: searchablePackagesNotChanged
         });
