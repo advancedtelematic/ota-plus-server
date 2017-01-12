deea57808302ffe56944dfce763bd8a71965fe0d
Mateusz <mplacek@concisesoftware.com>
2017-01-12 10:47:23 +0100

    Implemented new user profile, improved billing page

---
+++ ota-plus-web/app/assets/javascripts/components/user/edit-profile-billing.jsx
@@ -0,0 +1,112 @@
+define(function(require) {
+  var React = require('react'),
+      serializeForm = require('mixins/serialize-form'),
+      SotaDispatcher = require('sota-dispatcher'),
+      Responses = require('../responses');
+
+  class EditProfileBilling extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    handleSubmit(e) {
+      e.preventDefault();
+      var data = serializeForm(this.refs.form);
+      SotaDispatcher.dispatch({
+        actionType: 'update-user-billing',
+        data: data
+      });
+    }
+    render() {
+      return (
+        <div className="panel panel-grey">
+          <div className="panel-heading">Billing information</div>
+          <div className="panel-body">
+            <div className="col-xs-12">
+              <Responses 
+               action="update-user-billing" 
+               successText="Profile has been updated." 
+               errorText="Error occured during profile update."/>
+            </div>
+            <form ref="form" onSubmit={this.handleSubmit.bind(this)} id="form-personal-settings">
+              <div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-user-name">User name</label>
+                    <input type="text" className="form-control" name="nickname" id="input-user-name" required/>
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-company-name">Company name</label>
+                    <input type="text" className="form-control" name="company" id="input-company-name" required/>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-first-name">First name</label>
+                    <input type="text" className="form-control" name="firstname" id="input-first-name" required/>
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-last-name">Last name</label>
+                    <input type="text" className="form-control" name="lastname" id="input-last-name" required/>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-address">Address</label>
+                    <input type="text" className="form-control" name="address" id="input-address" required/>
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-city">City</label>
+                    <input type="text" className="form-control" name="city" id="input-city" required/>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-postal-code">Postal code</label>
+                    <input type="text" className="form-control" name="postalCode" id="input-postal-code" required/>
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-country">Country</label>
+                    <input type="text" className="form-control" name="country" id="input-country" required/>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-vat-number">Intracom VAT number</label>
+                    <input type="text" className="form-control" name="vat_number" id="input-vat-number" required/>
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label htmlFor="input-Email">Email</label>
+                    <input type="text" className="form-control" name="Email" id="input-Email" required/>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xs-12">
+                <button type="submit" className="btn btn-confirm pull-right">Update details</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return EditProfileBilling;
+});
--- ota-plus-web/app/assets/javascripts/components/user/edit-profile.jsx
+++ ota-plus-web/app/assets/javascripts/components/user/edit-profile.jsx
@@ -1,19 +1,17 @@
 define(function(require) {
   var React = require('react'),
-      Router = require('react-router'),
-      Link = Router.Link,
       db = require('stores/db'),
       SotaDispatcher = require('sota-dispatcher'),
       VelocityTransitionGroup = require('mixins/velocity/velocity-transition-group'),
       Loader = require('../loader'),
-      Responses = require('../responses'),
-      ReactI18next = require('reactI18next');
+      ReactI18next = require('reactI18next'),
+      EditProfileHeader = require('./edit-profile-header'),
+      EditProfileInfo = require('./edit-profile-info'),
+      EditProfileBilling = require('./edit-profile-billing');
 
-  class TestSettings extends React.Component {
+  class EditProfile extends React.Component {
     constructor(props) {
       super(props);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.changePassword = this.changePassword.bind(this);
       db.user.addWatch("poll-user-editprofile", _.bind(this.forceUpdate, this, null));
     }
     componentWillUnmount() {
@@ -21,10 +19,8 @@ define(function(require) {
     }
     handleSubmit(e) {
       e.preventDefault();
-      
       var data = {};
       data.name = this.refs.username.value;
-      
       SotaDispatcher.dispatch({
         actionType: 'update-user',
         data: data
@@ -36,54 +32,35 @@ define(function(require) {
         actionType: 'change-password',
       });
     }
-    render() {    
-      var user = db.user.deref();
+    render() {
+      const user = db.user.deref();
       const { t } = this.props;
       return (
-        <div className="row margin-top-70 ats">
-          <div className="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3">
-            <div className="ats-header text-center">
-              <p>{t('editprofile.title')}</p>
-            </div>
-            <VelocityTransitionGroup enter={{animation: "fadeIn"}} leave={{animation: "fadeOut"}}>
-              {!_.isUndefined(user) ?
-                <div className="row">
-                  <div className="col-lg-8 col-lg-offset-2">
-                    <Responses 
-                      action="update-user" 
-                      successText="Profile has been updated." 
-                      errorText="Error occured during profile update."/>
-                    <Responses 
-                      action="change-password" 
-                      successText="An email with password resetting instructions has been sent to your email account." 
-                      errorText="Error occured during password changing."/>
-                    <form>
-                      <div className="form-group">
-                        <input className="form-control" type="text" name="fullname" placeholder="full name" id="fullname" ref="username" defaultValue={user.fullName}/>
-                      </div>
-                      <div className="form-group margin-top-30">
-                        <button type="submit" className="ats-button" onClick={this.handleSubmit}>Save</button>
-                      </div>
-                      <div className="form-group">
-                        <Link to="/" className="ats-button grey">Cancel</Link>
-                      </div>
-                      <div className="form-group">
-                        <a href="#" className="ats-button bordered" onClick={this.changePassword}>Change password</a>
-                      </div>
-                    </form>
+        <span>
+          <VelocityTransitionGroup enter={{animation: "fadeIn", display: "flex"}} leave={{animation: "fadeOut", display: "flex"}} runOnMount={true}>
+            {!_.isUndefined(user) ?
+              <div className="content-wrapper">
+                <EditProfileHeader />
+                <div className="edit-profile-content">
+                  <div className="edit-profile-content-left">
+                     <EditProfileInfo 
+                      user={user}/>
+                   </div>
+                   <div className="edit-profile-content-right">
+                    <EditProfileBilling 
+                      user={user}/>
                   </div>
                 </div>
-              : undefined}
-            </VelocityTransitionGroup>
-    
-            {_.isUndefined(user) ?
-              <Loader />
+              </div>
             : undefined}
-          </div>
-        </div>
+          </VelocityTransitionGroup>
+          {_.isUndefined(user) ?
+            <Loader />
+           : undefined}
+        </span>
       );
     }
   };
 
-  return ReactI18next.translate()(TestSettings);
+  return ReactI18next.translate()(EditProfile);
 });
