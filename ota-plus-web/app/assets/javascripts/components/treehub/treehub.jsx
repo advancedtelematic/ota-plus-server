b80a26bd094bad7335c0bf202a24c9221dca634b
Mateusz <mplacek@concisesoftware.com>
2017-01-15 00:11:32 +0100

    Removed beta flag from Treehub

--- ota-plus-web/app/assets/javascripts/components/treehub/treehub.jsx
+++ ota-plus-web/app/assets/javascripts/components/treehub/treehub.jsx
@@ -1,13 +1,10 @@
 define(function(require) {
   var React = require('react'),
-      Router = require('react-router'),
-      Link = Router.Link,
       SotaDispatcher = require('sota-dispatcher'),
       db = require('stores/db'),
       VelocityTransitionGroup = require('mixins/velocity/velocity-transition-group'),
       ModalTooltip = require('../modal-tooltip'),
       TreehubHeader = require('./treehub-header'),
-      NoAccess = require('../noaccess'),
       Loader = require('../loader');
 
   class Treehub extends React.Component {
@@ -30,23 +27,16 @@ define(function(require) {
       window.addEventListener("resize", this.setContentHeight);
       this.setContentHeight();
     }
-    componentDidUpdate(prevProps, prevState) {
-      if(prevProps.hasBetaAccess !== this.props.hasBetaAccess) {
-        this.setContentHeight();
-      }
-    }
     componentWillUnmount() {
       window.removeEventListener("resize", this.setContentHeight);
       db.features.removeWatch("poll-features");
       db.features.reset();
     }
     setContentHeight() {    
-      if(this.props.hasBetaAccess) {
-        var windowHeight = jQuery(window).height();
-        this.setState({
-          contentHeight: windowHeight - jQuery('.grey-header').offset().top - jQuery('.grey-header').outerHeight()
-        });
-      }
+      var windowHeight = jQuery(window).height();
+      this.setState({
+        contentHeight: windowHeight - jQuery('.grey-header').offset().top - jQuery('.grey-header').outerHeight()
+      });
     }
     showTreehubTooltip(e) {
       if(e) e.preventDefault();
@@ -83,81 +73,78 @@ define(function(require) {
         </div>
       );
       return (
-        this.props.hasBetaAccess ?
-          <div>
-            <TreehubHeader />
-            <VelocityTransitionGroup enter={{animation: "fadeIn"}} leave={{animation: "fadeOut"}}>
-              {!_.isUndefined(this.state.features) ?
-                this.state.features.indexOf('treehub') < 0 ?
-                  <div className="treehub-disabled" style={{height: this.state.contentHeight}}>
-                    <div className="center-xy padding-15">
-                      <div className="font-22">Treehub not activated.</div>
-                      <div className="margin-top-10">
-                        <a href="#" className="font-18" onClick={this.showTreehubTooltip}>
-                          <span className="color-main"><strong>What is this?</strong></span>
-                        </a>
-                      </div>
+        <span>
+          <TreehubHeader />
+          <VelocityTransitionGroup enter={{animation: "fadeIn"}} leave={{animation: "fadeOut"}}>
+            {!_.isUndefined(this.state.features) ?
+              this.state.features.indexOf('treehub') < 0 ?
+                <div className="treehub-disabled" style={{height: this.state.contentHeight}}>
+                  <div className="center-xy padding-15">
+                    <div className="font-22">Treehub not activated.</div>
+                    <div className="margin-top-10">
+                      <a href="#" className="font-18" onClick={this.showTreehubTooltip}>
+                        <span className="color-main"><strong>What is this?</strong></span>
+                      </a>
                     </div>
                   </div>
-                : 
-                  <div className="treehub-enabled" style={{height: this.state.contentHeight}}>
-                    <div className="center-xy text-center padding-15">
-                      <span className="font-22"><strong>Welcome to TreeHub,</strong></span><br />
-                      the future of embedded device version management.<br />
-                      The first thing you'll need to do is add OSTree support into your project.<br /><br />
-                      Download your credentials and start pushing your images to TreeHub.
-                      <div className="margin-top-20">
-                        <a href="/api/v1/features/treehub/client" className="btn btn-confirm" target="_blank">Download</a>
-                      </div>
-                      <div className="margin-top-40">
-                        <div className="panel panel-grey">
-                          <div className="panel-heading">
-                            <img src="/assets/img/icons/white/treehub_leaf.png" alt=""/>
-                          </div>
-                          <div className="panel-body">
-                            I'm new to Yocto/Open Embedded. <br />
-                            Show me how to start a project <br />
-                            from scratch.
-                            <a href="http://docs.atsgarage.com/ostree/starting-a-new-ostreeenabled-yocto-project.html" className="btn btn-confirm btn-small">Start</a>
-                          </div>
-                        </div>
-                        <div className="panel panel-grey">
-                          <div className="panel-heading">
-                            <img src="/assets/img/icons/white/treehub_tree.png" alt=""/>
-                          </div>
-                          <div className="panel-body">
-                            I have an existing Yocto project <br />
-                            that I want to OTA-enable.
-                            <a href="http://docs.atsgarage.com/ostree/adding-ostree-to-your-existing-yocto-project.html" className="btn btn-confirm btn-small">Start</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-              : undefined}
-            </VelocityTransitionGroup>
-            {_.isUndefined(this.state.features) ? 
-              <div className="treehub-disabled" style={{height: this.state.contentHeight}}>
-                <div className="center-xy padding-15">
-                  <Loader />
                 </div>
-              </div>
+              : 
+                <div className="treehub-enabled" style={{height: this.state.contentHeight}}>
+                  <div className="center-xy text-center padding-15">
+                    <span className="font-22"><strong>Welcome to TreeHub,</strong></span><br />
+                    the future of embedded device version management.<br />
+                    The first thing you'll need to do is add OSTree support into your project.<br /><br />
+                    Download your credentials and start pushing your images to TreeHub.
+                    <div className="margin-top-20">
+                      <a href="/api/v1/features/treehub/client" className="btn btn-confirm" target="_blank">Download</a>
+                    </div>
+                    <div className="margin-top-40">
+                      <div className="panel panel-grey">
+                        <div className="panel-heading">
+                          <img src="/assets/img/icons/white/treehub_leaf.png" alt=""/>
+                        </div>
+                        <div className="panel-body">
+                          I'm new to Yocto/Open Embedded. <br />
+                          Show me how to start a project <br />
+                          from scratch.
+                          <a href="http://docs.atsgarage.com/ostree/starting-a-new-ostreeenabled-yocto-project.html" className="btn btn-confirm btn-small">Start</a>
+                        </div>
+                      </div>
+                      <div className="panel panel-grey">
+                        <div className="panel-heading">
+                          <img src="/assets/img/icons/white/treehub_tree.png" alt=""/>
+                        </div>
+                        <div className="panel-body">
+                          I have an existing Yocto project <br />
+                          that I want to OTA-enable.
+                          <a href="http://docs.atsgarage.com/ostree/adding-ostree-to-your-existing-yocto-project.html" className="btn btn-confirm btn-small">Start</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             : undefined}
-            <VelocityTransitionGroup enter={{animation: "fadeIn"}} leave={{animation: "fadeOut"}}>
-              {this.state.isTreehubTooltipShown ?
-                <ModalTooltip 
-                  title="TreeHub"
-                  body={tooltipContent}
-                  isCloseButtonShown={true}
-                  closeButtonLabel="Later"
-                  closeButtonAction={this.hideTreehubTooltip}
-                  confirmButtonLabel="Get started now!"
-                  confirmButtonAction={this.activateTreehub}/>
-              : undefined}
-            </VelocityTransitionGroup>
-          </div>
-        :
-          <NoAccess />
+          </VelocityTransitionGroup>
+          {_.isUndefined(this.state.features) ? 
+            <div className="treehub-disabled" style={{height: this.state.contentHeight}}>
+              <div className="center-xy padding-15">
+                <Loader />
+              </div>
+            </div>
+          : undefined}
+          <VelocityTransitionGroup enter={{animation: "fadeIn"}} leave={{animation: "fadeOut"}}>
+            {this.state.isTreehubTooltipShown ?
+              <ModalTooltip 
+                title="TreeHub"
+                body={tooltipContent}
+                isCloseButtonShown={true}
+                closeButtonLabel="Later"
+                closeButtonAction={this.hideTreehubTooltip}
+                confirmButtonLabel="Get started now!"
+                confirmButtonAction={this.activateTreehub}/>
+            : undefined}
+          </VelocityTransitionGroup>
+        </span>
       );
     }
   }
