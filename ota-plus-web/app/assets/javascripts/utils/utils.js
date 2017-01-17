3d308228175b1fce802955aff89b0cbbac7d94fc
Mateusz <mplacek@concisesoftware.com>
2017-01-17 13:51:06 +0100

    Improved toml configuration overlay

--- ota-plus-web/app/assets/javascripts/utils/utils.js
+++ ota-plus-web/app/assets/javascripts/utils/utils.js
@@ -6,7 +6,17 @@ define(function(require, exports, module) {
 		e.target.value = temp_value;
 	}
 
+	function limitToNumbersRange(min, max, e) {
+		var value = e.target.value;
+		if(min !== null)
+			value = Math.max(min, value);
+		if(max !== null)
+			value = Math.min(max, value);
+		e.target.value = value;
+	}
+
 	module.exports = { 
-		setFocusAtEnd
+		setFocusAtEnd,
+		limitToNumbersRange,
 	};
 });
