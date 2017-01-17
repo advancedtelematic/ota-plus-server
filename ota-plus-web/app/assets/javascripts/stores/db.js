69f56f80142c9f3001a4e982ed6c2ae375bc8569
Mateusz <mplacek@concisesoftware.com>
2017-01-17 18:06:03 +0100

    Implemented logic for provisioning feature

--- ota-plus-web/app/assets/javascripts/stores/db.js
+++ ota-plus-web/app/assets/javascripts/stores/db.js
@@ -44,6 +44,7 @@ define(function(require) {
       this.autoInstallPackagesForDevice = atom.createAtom();
       this.provisioningStatus = atom.createAtom();
       this.provisioningDetails = atom.createAtom();
+      this.provisioningCredentials = atom.createAtom();
       this.features = atom.createAtom();
       this.treehubJson = atom.createAtom();
       this.usagePerMonth = atom.createAtom();
