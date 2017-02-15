5daf815142c6d884c407025c523f033f5ff8bd52
Calum McCall <calum@advancedtelematic.com>
2017-02-15 13:59:36 +0100

    Update routing to use new device registry route
     - PRO-2549

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Version {
   val Akka = "2.4.7"
   val JsonWebSecurity = "0.2.1"
   val MockWs = "2.5.0"
-  val GeniviSota = "0.2.74"
+  val GeniviSota = "0.2.77"
   // Version 0.17 of akka-persistence-cassandra depends on Akka 2.4.7 and Scala 2.11.8
   // It is compatible with Cassandra 3.0.0 or higher
   // Details at https://github.com/akka/akka-persistence-cassandra
