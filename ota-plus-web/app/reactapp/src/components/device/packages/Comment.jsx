53f1c9c085bcea1e5940416447c8ad52337b6b1a
vhudyma <vhudyma@alliedcoders.com>
2017-04-24 10:34:56 +0300

    Fixed problem with blacklisting and comments.
    Fixed problem with blacklisting, when the last added blacklist comment appeared in blacklistmodal when blacklisting package and with comments, when the comment was not saved and you switched the package version(comment remained the same)

---
+++ ota-plus-web/app/reactapp/src/components/device/packages/Comment.jsx
@@ -0,0 +1,84 @@
+import React, { Component, PropTypes } from 'react';
+import { observer } from 'mobx-react';
+import { observe, observable } from 'mobx';
+import _ from 'underscore';
+
+@observer
+class Comment extends Component {
+	@observable activeEditField = false;
+    @observable showEditButton = false;
+    @observable commentFieldLength = 0;
+    @observable commentTmp = '';
+
+	constructor(props) {
+        super(props);
+        this.enableEditField = this.enableEditField.bind(this);
+        this.disableEditField = this.disableEditField.bind(this);
+        this.changeCommentFieldLength = this.changeCommentFieldLength.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+   	enableEditField(e) {
+   		if (e) e.preventDefault();
+        this.activeEditField = true;
+    }
+    disableEditField(e) {
+    	if (e) e.preventDefault();
+        var that = this;
+        setTimeout(function(){
+            if(document.activeElement.className.indexOf('accept-button') == -1) {
+                that.activeEditField = false;
+                that.commentTmp = null;
+            }
+        }, 1);
+    }
+    changeCommentFieldLength(e) {
+        var val = e.target.value;
+        this.commentFieldLength = val.length;
+        this.commentTmp = val;
+    }
+    handleSubmit(name, version, e) {
+    	if (e) e.preventDefault();
+        const data = {
+            name: name,
+            version: version,
+            details: {
+                description: this.commentTmp
+            }
+        };
+        this.props.packagesStore.updatePackageDetails(data);
+        this.commentTmp = null;
+        this.activeEditField = false;
+    }
+
+	render() {
+		const { version } = this.props;
+		return (
+			<div className="comments-section">
+				<textarea 
+		            className="input-comment" 
+		            name="comment" 
+		            value={this.commentTmp || version.description} 
+		            type="text" 
+		            placeholder="Comment here." 
+		            onKeyUp={this.changeCommentFieldLength.bind(this)}
+		            onChange={this.changeCommentFieldLength.bind(this)}
+		            onFocus={this.enableEditField.bind(this)}
+	            />
+	            {this.commentFieldLength > 0 && this.activeEditField ?
+	                <div className="action-buttons">
+	                    <a href="#" className="cancel-button" onClick={this.disableEditField}>
+	                        <img src="/assets/img/icons/close_icon.png" alt="" />
+	                    </a>
+	                    &nbsp;
+	                    <a href="#" className="accept-button" onClick={this.handleSubmit.bind(this, version.id.name, version.id.version)}>
+	                        <img src="/assets/img/icons/accept_icon.png" alt="" />
+	                    </a>
+	                </div>
+	            :
+	            	null}
+        	</div>	
+		);
+	}
+}
+
+export default Comment;
--- ota-plus-web/app/reactapp/src/components/device/packages/index.js
+++ ota-plus-web/app/reactapp/src/components/device/packages/index.js
@@ -3,11 +3,13 @@ import PackagesListItem from './ListItem';
 import PackagesListItemVersion from './ListItemVersion';
 import PackagesListItemOnDevice from './ListItemOnDevice';
 import PackagesDetails from './Details';
+import PackagesComment from './Comment';
 
 export {
     PackagesList,
     PackagesListItem,
     PackagesListItemVersion,
     PackagesListItemOnDevice,
-    PackagesDetails
+    PackagesDetails,
+    PackagesComment
 }
