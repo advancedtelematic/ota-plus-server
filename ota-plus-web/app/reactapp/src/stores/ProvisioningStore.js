0fc663e02866d25c53b93d03737f69a179fecbf2
vhudyma <vhudyma@alliedcoders.com>
2017-05-19 13:19:34 +0300

    Removed add new device button from access keys page

--- ota-plus-web/app/reactapp/src/stores/ProvisioningStore.js
+++ ota-plus-web/app/reactapp/src/stores/ProvisioningStore.js
@@ -108,7 +108,6 @@ export default class ProvisioningStore {
             .then(function (response) {
                 this.initialProvisioningKeys = response.data;
                 this.provisioningKeys = response.data;
-                this.provisioningKeys = [];
                 this._prepareProvisioningKeys();
                 this.provisioningKeysFetchAsync = handleAsyncSuccess(response);
             }.bind(this))
