11473bc93207d652c97bd91b92cc19a690ab0e0b
vhudyma <vhudyma@alliedcoders.com>
2017-05-22 11:58:31 +0300

    Fixed HW section

--- ota-plus-web/app/reactapp/src/components/device/hardware/List.jsx
+++ ota-plus-web/app/reactapp/src/components/device/hardware/List.jsx
@@ -8,39 +8,37 @@ class List extends Component {
     }
     render() {
         const { shownIds, showDetails, detailsIdShown, hardware } = this.props;
+        let dataId = 0;
+        if(!_.isUndefined(hardware.id) && (!_.isUndefined(hardware.description) || !_.isUndefined(hardware.class))) {
+            dataId = hardware['id-nr'];
+        }
         return (
             <span>
                 <div className="section-header">
                     Primary ECU
                 </div>
-                {!_.isUndefined(hardware.id) && (!_.isUndefined(hardware.description) || !_.isUndefined(hardware.class)) ?
-                    <a 
-                        href="#" 
-                        data-id={hardware['id-nr']}
-                        className={"selected" + (shownIds.indexOf(hardware['id-nr']) > -1 ? " shown" : "")}
-                        id="hardware-primary-details"
-                        onClick={e => e.preventDefault()}
-                    >
-                        <div className="desc">
-                            Serial: HGE3-123D-23KD-LDU3W922SW <br />
-                            Hardware ID: 123SSD-3FF2RR-5FNRHF
-                        </div>
-                        <i 
-                            className="fa fa-info hardware-info-icon" 
-                            onClick={showDetails}
-                            data-id={hardware['id-nr']}
-                        ></i>
-                    </a>
-                :
-                    <div className="not-available" id="hardware-primary-not-available">
-                        Not available
+                <a
+                    href="#" 
+                    data-id={dataId}
+                    className={"selected" + (shownIds.indexOf(dataId) > -1 ? " shown" : "")}
+                    id="hardware-primary-details"
+                    onClick={e => e.preventDefault()}
+                >
+                    <div className="desc">
+                        Serial: not reported <br />
+                        Hardware ID: not reported
                     </div>
-                }
+                    <i
+                        className="fa fa-info hardware-info-icon" 
+                        onClick={showDetails}
+                        data-id={dataId}
+                    ></i>
+                </a>
                 <div className="section-header">
                     Secondary ECU
                 </div>
                 <div className="not-available" id="hardware-secondary-not-available">
-                    Not available
+                    None reported
                 </div>
             </span>
         );
