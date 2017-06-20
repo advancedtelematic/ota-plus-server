ceebaa0c116deca5310f4d4030f0c4880892d620
vhudyma <vhudyma@alliedcoders.com>
2017-06-20 16:01:43 +0300

    Uncommented key fetching from endpoint

--- ota-plus-web/app/reactapp/src/stores/HardwareStore.js
+++ ota-plus-web/app/reactapp/src/stores/HardwareStore.js
@@ -49,27 +49,14 @@ export default class HardwareStore {
 
     fetchPublicKey(deviceId, ecuId) {
         resetAsync(this.hardwarePublicKeyFetchAsync, true);
-        // return axios.get(`${API_ECUS_PUBLIC_KEY_FETCH}/${deviceId}/ecus/public_key/ecu_serial=${ecuId}`)
-        //     .then(function (response) {
-        //         this.publicKey = response.data;
-        //         this.hardwarePublicKeyFetchAsync = handleAsyncSuccess(response);
-        //     }.bind(this))
-        //     .catch(function (error) {
-        //         this.hardwarePublicKeyFetchAsync = handleAsyncError(error);
-        //     }.bind(this));
-        setTimeout(() => {
-            this.publicKey = {
-                "keytype": "RSA",
-                "keyval": {
-                    "public": `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqGKukO1De7zhZj6+H0qtjTkVxwTCpvKe4eCZ0
-FPqri0cb2JZfXJ/DgYSF6vUpwmJG8wVQZKjeGcjDOL5UlsuusFncCzWBQ7RKNUSesmQRMSGkVb1/
-3j+skZ6UtW+5u09lHNsj6tQ51s1SPrCBkedbNf0Tp0GbMJDyR4e9T04ZZwIDAQAB
------END PUBLIC KEY-----`
-                }
-            };
-            this.hardwarePublicKeyFetchAsync = handleAsyncSuccess({});
-        }, 2000);
+        return axios.get(`${API_ECUS_PUBLIC_KEY_FETCH}/${deviceId}/ecus/public_key/ecu_serial=${ecuId}`)
+            .then(function (response) {
+                this.publicKey = response.data;
+                this.hardwarePublicKeyFetchAsync = handleAsyncSuccess(response);
+            }.bind(this))
+            .catch(function (error) {
+                this.hardwarePublicKeyFetchAsync = handleAsyncError(error);
+            }.bind(this));
     }
 
     _reset() {
