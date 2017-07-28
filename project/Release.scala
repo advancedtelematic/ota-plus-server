71ccc55a18b991998062482f6c7de090527f7863
Jerry Trieu <jerry@advancedtelematic.com>
2017-07-28 15:46:28 +0200

    Remove version tag in Release

--- project/Release.scala
+++ project/Release.scala
@@ -3,66 +3,18 @@ object Release {
   import sbt.Keys._
   import sbt._
 
-  // we hide the existing definition for setReleaseVersion to replace it with our own
-
-  import sbtrelease.ReleaseStateTransformations.{setReleaseVersion => _, _}
+  import sbtrelease.ReleaseStateTransformations._
   import sbtrelease.ReleasePlugin.autoImport._
   import sbtrelease._
 
-  def setVersionOnly(selectVersion: Versions => String): ReleaseStep = { st: State =>
-    val vs = st.get(ReleaseKeys.versions).getOrElse(sys.error("No versions are set! Was this release part executed before inquireVersions?"))
-    val selected = selectVersion(vs)
-
-    st.log.info("Setting version to '%s'." format selected)
-    val useGlobal = Project.extract(st).get(releaseUseGlobalVersion)
-    val versionStr = (if (useGlobal) globalVersionString else versionString) format selected
-
-    reapply(Seq(
-      if (useGlobal) version in ThisBuild := selected
-      else version := selected
-    ), st)
-  }
-
-  lazy val setReleaseVersion: ReleaseStep = setVersionOnly(_._1)
-
-  val showNextVersion = settingKey[String]("the future version once releaseNextVersion has been applied to it")
-  val showReleaseVersion = settingKey[String]("the version once releaseVersion has been applied to it")
-
-  val skipTag = scala.util.Properties.envOrElse("SKIP_TAG", "false")
-
-  def isTag(rs: Object) = {
-    rs == releaseStepCommand("docker:publishLocal")
-  }
-
-  val commonReleaseSteps: Seq[sbtrelease.ReleasePlugin.autoImport.ReleaseStep] =
-    Seq(
-      checkSnapshotDependencies,
-      releaseStepCommand(ExtraReleaseCommands.initialVcsChecksCommand),
-      inquireVersions,
-      setReleaseVersion,
-      releaseStepCommand("mkVersionProperties"),
-      releaseStepCommand("docker:publishLocal"),
-      releaseStepCommand("docker:publish")
-    )
-
-  val releaseSteps =
-    if (skipTag == "true") {
-      commonReleaseSteps ++ Seq(pushChanges)
-    } else {
-      commonReleaseSteps ++ Seq(tagRelease, pushChanges)
-    }
   lazy val settings = Seq(
-    showReleaseVersion <<= (version, releaseVersion)((v,f)=>f(v)),
-    showNextVersion <<= (version, releaseNextVersion)((v,f)=>f(v)),
-
-    releaseVersion <<= (releaseVersionBump)( bumper=>{
-      ver => Version(ver)
-          .map(_.withoutQualifier)
-          .map(_.bump(bumper).string).getOrElse(versionFormatError)
-    }),
 
     releaseIgnoreUntrackedFiles := true,
 
-    releaseProcess := releaseSteps
+    releaseProcess := Seq(
+      checkSnapshotDependencies,
+      releaseStepCommand("mkVersionProperties"),
+      releaseStepCommand("docker:publish")
+    )
   )
 }
