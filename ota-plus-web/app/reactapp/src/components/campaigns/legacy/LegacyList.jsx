b40ec7849fa4390a1c92119ce23ecf90074933bd
vhudyma <vhudyma@alliedcoders.com>
2017-08-31 09:53:33 +0300

    Transfered campaigns tab to home page

--- ota-plus-web/app/reactapp/src/components/campaigns/legacy/LegacyList.jsx
+++ ota-plus-web/app/reactapp/src/components/campaigns/legacy/LegacyList.jsx
@@ -11,75 +11,115 @@ class LegacyList extends Component {
         super(props);
     }
     render() {
-        const { campaignsStore, groupsStore, addNewWizard, showWizard, showRenameModal, goToCampaignDetails } = this.props;
+        const { campaignsStore, groupsStore, showRenameModal, goToCampaignDetails, onHomePage } = this.props;
         return (
             <span>
                 {campaignsStore.preparedLegacyCampaigns.length ?
                     <div className="legacy-list">
-                        <div className="section-header">
-                            Running campaigns
-                        </div>
-                        <div className="campaigns-list">
-                            {campaignsStore.runningLegacyCampaigns.length ?
-                                <span>
-                                    <div className="heading">
-                                        <div></div>
-                                        <div className="column">Name</div>
-                                        <div className="column">Start date</div>
-                                        <div className="column">End date</div>
-                                        <div className="column"></div>
-                                        <div className="column"></div>
-                                    </div>
-                                    {_.map(campaignsStore.runningLegacyCampaigns, (campaign) => {
-                                        return (
-                                            <CampaignsLegacyListItem 
-                                                groupsStore={groupsStore}
-                                                goToCampaignDetails={goToCampaignDetails}
-                                                showRenameModal={showRenameModal}
-                                                campaign={campaign}
-                                                key={campaign.id}
-                                            />
-                                        );
-                                    })}
-                                </span>
-                            :
-                                <div className="empty">
-                                    No running campaigns.
+                        {!onHomePage ?
+                            <span>
+                                <div className="section-header">
+                                    Running campaigns
                                 </div>
-                            }
-                        </div>
-                        <div className="section-header">
-                            Finished campaigns
-                        </div>
-                        <div className="campaigns-list">
-                            {campaignsStore.finishedLegacyCampaigns.length ?
-                                <span>
-                                    <div className="heading">
-                                        <div></div>
-                                        <div className="column">Name</div>
-                                        <div className="column">Start date</div>
-                                        <div className="column">End date</div>
-                                        <div className="column"></div>
-                                        <div className="column"></div>
-                                    </div>
-                                    {_.map(campaignsStore.finishedLegacyCampaigns, (campaign) => {
-                                        return (
-                                            <CampaignsLegacyListItem 
-                                                groupsStore={groupsStore}
-                                                goToCampaignDetails={goToCampaignDetails}
-                                                showRenameModal={showRenameModal}
-                                                campaign={campaign}
-                                                key={campaign.id}
-                                            />
-                                        );
-                                    })}
-                                </span>
-                            :
-                                <div className="empty">
-                                    No finished campaigns.
+                                <div className="campaigns-list">
+                                    {campaignsStore.runningLegacyCampaigns.length ?
+                                        <span>
+                                            <div className="heading">
+                                                <div></div>
+                                                <div className="column">Name</div>
+                                                <div className="column">Start date</div>
+                                                <div className="column">End date</div>
+                                                <div className="column"></div>
+                                                <div className="column"></div>
+                                            </div>
+                                            {_.map(campaignsStore.runningLegacyCampaigns, (campaign) => {
+                                                return (
+                                                    <CampaignsLegacyListItem 
+                                                        groupsStore={groupsStore}
+                                                        goToCampaignDetails={goToCampaignDetails}
+                                                        showRenameModal={showRenameModal}
+                                                        campaign={campaign}
+                                                        onHomePage={onHomePage}
+                                                        key={campaign.id}
+                                                    />
+                                                );
+                                            })}
+                                        </span>
+                                    :
+                                        <div className="empty">
+                                            No running campaigns.
+                                        </div>
+                                    }
                                 </div>
-                            }
-                        </div>
+                                <div className="section-header">
+                                    Finished campaigns
+                                </div>
+                                <div className="campaigns-list">
+                                    {campaignsStore.finishedLegacyCampaigns.length ?
+                                        <span>
+                                            <div className="heading">
+                                                <div></div>
+                                                <div className="column">Name</div>
+                                                <div className="column">Start date</div>
+                                                <div className="column">End date</div>
+                                                <div className="column"></div>
+                                                <div className="column"></div>
+                                            </div>
+                                            {_.map(campaignsStore.finishedLegacyCampaigns, (campaign) => {
+                                                return (
+                                                    <CampaignsLegacyListItem 
+                                                        groupsStore={groupsStore}
+                                                        goToCampaignDetails={goToCampaignDetails}
+                                                        showRenameModal={showRenameModal}
+                                                        campaign={campaign}
+                                                        onHomePage={onHomePage}
+                                                        key={campaign.id}
+                                                    />
+                                                );
+                                            })}
+                                        </span>
+                                    :
+                                        <div className="empty">
+                                            No finished campaigns.
+                                        </div>
+                                    }
+                                </div>
+                            </span>
+                        :
+                            <span>
+                                <div className="campaigns-list">
+                                    {campaignsStore.runningLegacyCampaigns.length ?
+                                        <span>
+                                            <div className="heading">
+                                                <div></div>
+                                                <div className="column">Name</div>
+                                                <div className="column">Start date</div>
+                                                <div className="column">End date</div>
+                                                <div className="column"></div>
+                                                <div className="column"></div>
+                                            </div>
+                                            {_.map(campaignsStore.runningLegacyCampaigns, (campaign) => {
+                                                return (
+                                                    <CampaignsLegacyListItem 
+                                                        groupsStore={groupsStore}
+                                                        goToCampaignDetails={goToCampaignDetails}
+                                                        showRenameModal={showRenameModal}
+                                                        campaign={campaign}
+                                                        onHomePage={onHomePage}
+                                                        key={campaign.id}
+                                                    />
+                                                );
+                                            })}
+                                        </span>
+                                    :
+                                        <div className="wrapper-center">
+                                            No running campaigns.
+                                        </div>
+                                    }
+                                </div>
+                            </span>
+                        }
+                        
                     </div>
                 : 
                     <span className="content-empty">
@@ -93,12 +133,7 @@ class LegacyList extends Component {
     }
 }
 
-LegacyList.contextTypes = {
-    router: React.PropTypes.object.isRequired
-}
-
 LegacyList.propTypes = {
-    
 }
 
 export default LegacyList;
