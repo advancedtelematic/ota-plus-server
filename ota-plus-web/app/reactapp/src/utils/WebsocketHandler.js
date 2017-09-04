ad89d9d906af7052aabf41a8ced62629a4a0e9b3
vhudyma <vhudyma@alliedcoders.com>
2017-09-04 15:51:50 +0300

    Installation history report refreshes automatically

--- ota-plus-web/app/reactapp/src/utils/WebsocketHandler.js
+++ ota-plus-web/app/reactapp/src/utils/WebsocketHandler.js
@@ -43,6 +43,7 @@ const WebsocketHandler = (function (wsUrl, stores) {
                 case "UpdateSpec":
                     if(window.location.href.indexOf('/device/') > -1 && stores.devicesStore.device.isDirector && stores.devicesStore.device.uuid === data.device) {
                         stores.devicesStore.fetchMultiTargetUpdates(data.device);
+                        stores.packagesStore.fetchDirectorDevicePackagesHistory(data.device, stores.packagesStore.directorDevicePackagesFilter, true);
                     }
                     if(stores.packagesStore.deviceQueue.length && stores.devicesStore.device.uuid === data.device) {
                         if(data.status !== 'Pending') {
