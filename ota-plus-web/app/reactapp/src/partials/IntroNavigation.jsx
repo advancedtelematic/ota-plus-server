257f81a60fc0cefa3aa31db4a066b6951a590a94
vhudyma <vhudyma@alliedcoders.com>
2017-09-08 13:35:30 +0300

    Showed ota plus tabs when user sees intro navigation

--- ota-plus-web/app/reactapp/src/partials/IntroNavigation.jsx
+++ ota-plus-web/app/reactapp/src/partials/IntroNavigation.jsx
@@ -10,11 +10,43 @@ class IntroNavigation extends Component {
     }
     render() {
         const { userStore, devicesStore, logoLink, hideQueueModal, toggleOtaPlusMode, otaPlusMode, alphaPlusEnabled } = this.props;
+        const otaPlusNavigation = (
+            <ul className="nav navbar-nav">
+                <li>
+                    <Link to="/fleet" activeClassName="active" id="link-fleet">Fleet</Link>
+                </li>
+                <li>
+                    <Link to="/software-repository" activeClassName="active" id="link-software-repository">Software repository</Link>
+                </li>
+                <li>
+                    <Link to="/features" activeClassName="active" id="link-features">Features</Link>
+                </li>
+                <li>
+                    <Link to="/advanced-campaigns" activeClassName="active" id="link-advanced-campaigns">Advanced campaigns</Link>
+                </li>
+                <li>
+                    <Link to="/connectors" activeClassName="active" id="link-connectors">Connectors</Link>
+                </li>
+                <li>
+                    <Link to="/gateway" activeClassName="active" id="link-gateway">Gateway</Link>
+                </li>
+                <li>
+                    <Link to="/auditor" activeClassName="active" id="link-uditor">Auditor</Link>
+                </li>
+          </ul>
+        );
         return (
             <nav className="navbar navbar-inverse navbar-fixed-top">
                   <div className="container">
                     <div className="navbar-header">
                           <Link to={logoLink} className="navbar-brand" id="logo"></Link>
+                    </div>
+                    <div id="navbar">
+                        {otaPlusMode ?
+                            otaPlusNavigation
+                        :
+                            null
+                        }
                     </div>
                    <ul className="right-nav">
                         <li className="text-link">
