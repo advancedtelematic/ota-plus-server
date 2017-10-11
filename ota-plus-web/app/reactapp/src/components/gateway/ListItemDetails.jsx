8e53b7764529f147fc136a43dbf74c83ead091b8
vhudyma <vhudyma@alliedcoders.com>
2017-10-11 11:13:07 +0300

    Made improvements on gateway page

--- ota-plus-web/app/reactapp/src/components/gateway/ListItemDetails.jsx
+++ ota-plus-web/app/reactapp/src/components/gateway/ListItemDetails.jsx
@@ -3,12 +3,7 @@ import { observer } from 'mobx-react';
 import _ from 'underscore';
 import moment from 'moment';
 import GatewayStats from './Stats';
-
-const currentHour = moment().hour();
-const currentMinutes = moment().minutes();
-const currentDay = moment().format("MMM Do");
-const nextDay = moment().add(1, 'days').format("MMM Do");
-
+import BarChart from './Barchart';
 
 @observer
 class ListItemDetails extends Component {
@@ -184,108 +179,9 @@ class ListItemDetails extends Component {
                                         <span className="text">{item.connections.avg}</span>
                                     </div>
                                 </div>
-                                <div className="bar-chart">
-                                    <div className="br">
-                                        {_.map(item.connections.live, (count, hour) => {
-                                            let bgColor = hour < currentHour ? '#e7a539' : 'grey';
-                                            let percentageFilled = (hour == currentHour) ? currentMinutes / 60 * 100 : 100;
-                                            let limit = item.connections.limit.split('.').join("");
-
-                                            return (
-                                                <div className={"bar" + (count === 0 ? " empty" : "")} 
-                                                     style={{
-                                                        height: count / limit * 100 + "%",
-                                                        backgroundColor: bgColor,
-                                                     }}
-                                                     key={hour}
-                                                     data-hour={hour}
-                                                >
-                                                    {percentageFilled !== 100 ?
-                                                        <div className="bar-wrapper"
-                                                             style={{
-                                                                height: percentageFilled + "%",
-                                                                backgroundColor: '#e7a539',
-                                                                position: 'absolute',
-                                                                bottom: 0,
-                                                                left: 0,
-                                                                right: 0
-                                                             }}>
-                                                        </div>
-                                                    :
-                                                        null
-                                                    }
-                                                    {hour % 3 === 0  ?
-                                                        <a href="#" className="anchor">
-                                                        </a>
-                                                    :
-                                                        null
-                                                    }
-                                                </div>
-                                            );                                        
-                                        })}
-                                        <a href="#" className="anchor">
-                                        </a>
-                                    </div>                                    
-                                    <div className="x-axis">
-                                        <div className="time">
-                                            <div className="value">
-                                                {currentDay}
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                03AM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                06AM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                09AM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                12PM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                03PM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                06PM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                09PM
-                                            </div>
-                                        </div>
-                                        <div className="time">
-                                            <div className="value">
-                                                {nextDay}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="y-axis">
-                                        <div className="devices">
-                                            <div className="value" style={{'left': item.connections.limit.length > 3 ? '-28px' : '-20px'}}>
-                                                {item.connections.limit}
-                                            </div>
-                                        </div>
-                                        <div className="devices">
-                                            <div className="value" style={{'left': item.connections.limit.length > 3 ? '-28px' : '-20px'}}>
-                                                {(item.connections.limit.split('.').join("") / 2).toLocaleString()}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <BarChart
+                                    connections={item.connections}
+                                />
                                 <div className="stats">
                                     <ul>
                                         <li>
