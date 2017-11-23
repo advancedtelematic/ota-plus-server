6f17a379047a4791d4bd3cfd870c580ddaa65956
Simão Mata <simao@advancedtelematic.com>
2017-11-23 17:48:52 +0100

    Support login without any authentication
    This **MUST NOT** be used in production, this is used for testing or
    demoing the app.
    
    When activated, this allows a user to login by simply visiting the
    app. A fake token is created and saved into the Play session. The
    session will be read by the server on subsequent requests.
    
    For this to work, the following some settings need to be set in
    `application.conf` or via environment variables. See
    `GarageNoLoginSpec.scala` to check which settings need to be changed.
    
    Secure cookies need to be disabled, or the app needs to be running
    with https. If using unsecure cookies, you'll need to change
    `WS_PROTOCOL` and possibly other settings to make this work.
    
    The dummy access token is signed using the secret stored in
    `application.conf` or via the `JWT_SECRET` environment variable. This
    means that the token can be used to authenticate against other
    services that are using local verification without auth+. This is
    typically set by using `AUTH_VERIFICATION=local`.

---
+++ ota-plus-web/test/com/advancedtelematic/controllers/GarageNoLoginSpec.scala
@@ -0,0 +1,79 @@
+package com.advancedtelematic.controllers
+
+import _root_.akka.stream.Materializer
+import com.advancedtelematic.auth.UiAuthAction
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
+import com.advancedtelematic.auth.garage.NoLoginAction
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class GarageNoLoginSpec extends PlaySpec with GuiceOneServerPerSuite with Results {
+  val application = new GuiceApplicationBuilder()
+    .configure("authplus.token" -> "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow")
+    .configure("oidc.namespace" -> "test-namespace")
+    .configure("oidc.loginAction" -> "com.advancedtelematic.auth.garage.NoLoginAction")
+    .configure("oidc.tokenVerification" -> "com.advancedtelematic.auth.oidc.NoTokenIntrospection")
+    .build
+
+  implicit val mat = application.injector.instanceOf[Materializer]
+  implicit val ec = application.injector.instanceOf[ExecutionContext]
+
+  val BodyParser: PlayBodyParsers = PlayBodyParsers()
+
+  val Action: DefaultActionBuilder = DefaultActionBuilder(BodyParser.anyContent)
+
+  val authAction = application.injector.instanceOf[UiAuthAction]
+
+  val loginAction = application.injector.instanceOf[NoLoginAction]
+
+  def fakeRoute() : Action[AnyContent] = authAction.async { implicit request =>
+    Future.successful(Ok(""))
+  }
+
+  "UiAuthAction" should {
+    "redirect to /login when no session exists" in {
+      val request = FakeRequest(GET, "/")
+      val result = call(fakeRoute(), request)
+      status(result) must be(303)
+
+    }
+
+    "accept requests with a valid session" in {
+      val request = FakeRequest(GET, "/login")
+      val result = call(loginAction, request)
+      status(result) must be(303)
+
+      val authenticatedRequest = FakeRequest(GET, "/").withSession(session(result).data.toList:_*)
+      val authenticatedResult = call(fakeRoute(), authenticatedRequest)
+
+      status(authenticatedResult) must be(200)
+    }
+  }
+
+  "NoLoginAction" should {
+    "set a session on GET /login" in {
+      val request = FakeRequest(GET, "/login")
+      val result = call(loginAction, request)
+      status(result) must be(303)
+      redirectLocation(result) must contain("/")
+
+      session(result).data.keys must contain("id_token")
+      session(result).data.keys must contain("namespace")
+      session(result).data.keys must contain("access_token")
+    }
+
+    "set namespace from config" in {
+      val request = FakeRequest(GET, "/login")
+      val result = call(loginAction, request)
+      status(result) must be(303)
+      redirectLocation(result) must contain("/")
+
+      session(result).data("namespace") must be("test-namespace")
+    }
+  }
+}
