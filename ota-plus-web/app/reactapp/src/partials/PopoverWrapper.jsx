efd4ca8673d786f55522b31fec6ed98d3d647039
vhudyma <vhudyma@alliedcoders.com>
2018-02-05 10:05:01 +0200

    Fixed an issue with not displaying installed packs

--- ota-plus-web/app/reactapp/src/partials/PopoverWrapper.jsx
+++ ota-plus-web/app/reactapp/src/partials/PopoverWrapper.jsx
@@ -18,10 +18,6 @@ class PopoverWrapper extends Component {
         this.handleCopy = this.handleCopy.bind(this);
     }
     handleTouchTap(e) {
-        if(e) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
         const { onOpen } = this.props;
         this.anchorEl = e.currentTarget;
         this.popoverShown = true;
