6de56d60b7066f0fe21d905aba78b93c3f80bb9d
vhudyma <vhudyma@alliedcoders.com>
2018-04-11 14:59:31 +0300

    Replaced check icon in CDV and Edit profile section

--- ota-plus-web/app/reactapp/src/components/device/packages/index.js
+++ ota-plus-web/app/reactapp/src/components/device/packages/index.js
@@ -1,11 +1,9 @@
 import PackagesListItem from './ListItem';
 import PackagesListItemVersion from './ListItemVersion';
-import PackagesComment from './Comment';
 import PackagesList from './List';
 
 export {
     PackagesListItem,
     PackagesListItemVersion,
-    PackagesComment,
     PackagesList
 }
