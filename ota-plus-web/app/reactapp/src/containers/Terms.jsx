95448ab9ed162143c38c752c553ddca634df7eb7
dfedko <dfedko@alliedcoders.com>
2018-05-15 09:21:08 -0400

    Fixed T&C acceptance issues & style changes

--- ota-plus-web/app/reactapp/src/containers/Terms.jsx
+++ ota-plus-web/app/reactapp/src/containers/Terms.jsx
@@ -9,22 +9,27 @@ import * as contracts from '../../../assets/contracts/';
 @observer
 export default class Terms extends Component {
     @observable termsAccepted = false;
+    @observable showModal = false;
 
     componentWillMount() {
         this.props.userStore.fetchContracts();
     }
-
+    toggleModal(e) {
+        e.preventDefault();
+        this.showModal = !this.showModal;
+    }
     render() {
         const {setTermsAccepted, userStore} = this.props;
-        const terms = _.find(userStore.contracts, (obj) => obj.contract === 'v1_en.html');
+        let terms = _.find(userStore.contracts, (obj) => contracts.default[obj.contract]);
+        !terms ? terms = {contract: 'terms_conditions.html'}: terms;
         const htmlDoc = terms && terms.contract  ? {__html: contracts.default[terms.contract]} : null;
         const contractModalContent = (
             <div className="modal-wrapper">
-                <div dangerouslySetInnerHTML={htmlDoc}>
+                <div className="overflow" dangerouslySetInnerHTML={htmlDoc}>
                 </div>
-                <div className="body-actions">
-                    <button className="back btn-primary left"><a href="/login">Back</a></button>
-                    <button className="btn-primary left" onClick={() => {setTermsAccepted(terms.contract)}}>
+                <div className="body-actions" style={{display:'flex', justifyContent: 'space-between'}}>
+                    <button className="back btn-primary" id="modal-back-button"><a href="#" onClick={this.toggleModal.bind(this)}>Back</a></button>
+                    <button className="btn-primary" id="modal-accept-button"> onClick={() => setTermsAccepted(terms.contract)}>
                         I agree
                     </button>
                 </div>
@@ -36,26 +41,31 @@ export default class Terms extends Component {
                     <div className="logo">
                         <img src="/assets/img/HERE_pos.png" alt="HERE"/>
                     </div>
-                    <div className="title">
-                        Terms and conditions
+                    <div className="title terms-title">
+                        This is a 90 days trial for
+                        internal evaluation purposes only
                     </div>
                     <div className="checkbox-wrapper">
-                        <button className={`btn-checkbox ${this.termsAccepted ? 'checked': ''}`} onClick={() => this.termsAccepted = !this.termsAccepted}>
-                            <i className="fa fa-check" aria-hidden="true"></i>
+                        <button className={`btn-checkbox ${this.termsAccepted ? 'checked': ''}`} onClick={() => this.termsAccepted = !this.termsAccepted} id={"terms-checkbox" + (this.termsAccepted ? '-checked' : '')}>
+                            <i className="fa fa-check" aria-hidden="true"/>
                         </button>
-                        <p>I AGREE TO OUR <a href="#">SERVICE TERMS</a> AND <Link to="/policy">PRIVACY POLICY.</Link></p>
+                        <p>
+                            I agree to HERE Location Platform Services Online
+                            <a id="service-terms-link" target="_blank" onClick={this.toggleModal.bind(this)} href="#"> terms and conditions </a>
+                            and <a target="_blank" id="privacy-policy-link" href="https://legal.here.com/en-gb/privacy" >privacy policy.</a>
+                        </p>
                     </div>
                     <div className="steps">
-                        <button className="back btn-primary"><a href="/login">Back</a></button>
+                        <button className="back btn-primary" id="terms-btn-back"><a href="/login">Back</a></button>
                         {this.termsAccepted ?
-                            <button className="next btn-primary" onClick={() => {setTermsAccepted('v1_en.html')}}>Continue</button> :
-                            <button className="next btn-primary" disabled>Continue</button>
+                            <button className="next btn-primary" id="terms-btn-continue" onClick={() => {setTermsAccepted(terms && terms.contract)}}>Continue</button> :
+                            <button className="next btn-primary" id="terms-btn-continue_disabled" disabled>Continue</button>
                         }
                     </div>
                 </div>
                 <Modal
                     content={contractModalContent}
-                    shown={!!userStore.contracts.length && !_.isNull(htmlDoc)}
+                    shown={this.showModal}
                 />
             </div>
         )
