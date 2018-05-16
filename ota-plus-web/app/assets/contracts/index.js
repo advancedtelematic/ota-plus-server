22971124212fe85c2fa54f9eff4c616d0da53c3b
vhudyma <vhudyma@alliedcoders.com>
2018-05-16 13:29:47 +0300

    Fixed terms and conditions name

--- ota-plus-web/app/assets/contracts/index.js
+++ ota-plus-web/app/assets/contracts/index.js
@@ -1,5 +1,7 @@
 import terms_conditions from './tc/2018-01/en/terms_conditions.html';
 
+export const defaultName = 'tc/2018-01/en/terms_conditions.html';
+
 export default {
-    'terms_conditions.html': terms_conditions
+    'tc/2018-01/en/terms_conditions.html': terms_conditions
 }
