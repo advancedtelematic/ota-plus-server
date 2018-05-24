3dffbe48ceee4a1aeb0722d3dadd0837fccc3c58
dfedko <dfedko@alliedcoders.com>
2018-05-24 04:40:36 -0400

    [PRO-5389] Change terms of use page content

--- ota-plus-web/app/reactapp/src/containers/Terms.jsx
+++ ota-plus-web/app/reactapp/src/containers/Terms.jsx
@@ -4,6 +4,7 @@ import {observer} from 'mobx-react';
 import { Link } from 'react-router';
 import {Modal} from '../partials';
 import _ from 'underscore';
+import moment from 'moment';
 import * as contracts from '../../../assets/contracts/';
 
 @observer
@@ -16,8 +17,9 @@ export default class Terms extends Component {
         this.showModal = !this.showModal;
     }
     render() {
-        const {setTermsAccepted, userStore} = this.props;
+        const {setTermsAccepted, userStore, backButtonAction, checked} = this.props;
         let terms = _.find(userStore.contracts, (obj) => contracts.default[obj.contract]);
+        let agreedDate = terms && terms.accepted;
         !terms ? terms = {contract: contracts.defaultName}: terms;
         const htmlDoc = terms && terms.contract  ? {__html: contracts.default[terms.contract]} : null;
         const contractModalContent = (
@@ -42,18 +44,23 @@ export default class Terms extends Component {
                         This is a 90 days trial for
                         internal evaluation purposes only
                     </div>
+                    <p className="subtitle--terms">
+                        The proprietary software that you upload to HERE OTA Connect SaaS will be used by HERE only for
+                        the purposes of allowing you to test the service internally during the Trial Period.
+                    </p>
                     <div className="checkbox-wrapper">
-                        <button className={`btn-checkbox ${this.termsAccepted ? 'checked': ''}`} onClick={() => this.termsAccepted = !this.termsAccepted} id={"terms-checkbox" + (this.termsAccepted ? '-checked' : '')}>
+                        <button className={`btn-checkbox ${this.termsAccepted || checked ? 'checked': ''}`} onClick={() => {!checked ? this.termsAccepted = !this.termsAccepted : null}} id={"terms-checkbox" + (this.termsAccepted ? '-checked' : '')}>
                             <i className="fa fa-check" aria-hidden="true"/>
                         </button>
                         <p>
                             I agree to HERE Location Platform Services Online
                             <a id="service-terms-link" target="_blank" href="https://developer.here.com/terms-and-conditions"> terms and conditions </a>
-                            and <a target="_blank" id="privacy-policy-link" href="https://legal.here.com/en-gb/privacy" >privacy policy.</a>
+                            and <a target="_blank" id="privacy-policy-link" href="https://legal.here.com/en-gb/privacy" >privacy policy</a>
+                            <span className="agreed--terms">{checked ? ` (AGREED ON ${moment(agreedDate).format('MMM Do YYYY')})` : '.'}</span>
                         </p>
                     </div>
                     <div className="steps">
-                        <button className="back btn-primary" id="terms-btn-back"><a href="/login">Back</a></button>
+                        <a href="/login" className="back btn-primary" id="terms-btn-back" onClick={backButtonAction ? backButtonAction : ''}>Back</a>
                         {this.termsAccepted ?
                             <button className="next btn-primary" id="terms-btn-continue" onClick={() => {setTermsAccepted(terms && terms.contract)}}>Continue</button> :
                             <button className="next btn-primary" id="terms-btn-continue_disabled" disabled>Continue</button>
--- ota-plus-web/app/reactapp/src/pages/TermsAndConditions.jsx
+++ ota-plus-web/app/reactapp/src/pages/TermsAndConditions.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import { MetaData, FadeAnimation } from '../utils';
+import { Terms } from '../containers';
 import * as contracts from '../../../assets/contracts/';
 import _ from 'underscore';
 
@@ -12,23 +13,28 @@ class TermsAndConditions extends Component {
         super(props);
     }
     render() {
-        const { userStore } = this.props;
+        const { userStore, backButtonAction } = this.props;
         let terms = _.find(userStore.contracts, (obj) => contracts.default[obj.contract]);
         const htmlDoc = terms && terms.contract  ? {__html: contracts.default[terms.contract]} : null;
+        const oldTerms = (
+            <div>
+                <div className="header">
+                    General terms and conditions
+                </div>
+                <div className="wrapper">
+                    <div className="content-header">
+                        <span>Privacy Policy</span>
+                        <span>Effective date: <span className="date">Friday, 4 December, 2015</span></span>
+                    </div>
+                    <div className="content" dangerouslySetInnerHTML={htmlDoc}>
+                    </div>
+                </div>
+            </div>
+        )
         return (
             <FadeAnimation>
                 <MetaData title={title}>
-                    <div className="header">
-                        General terms and conditions
-                    </div>
-                    <div className="wrapper">
-                        <div className="content-header">
-                            <span>Privacy Policy</span>
-                            <span>Effective date: <span className="date">Friday, 4 December, 2015</span></span>
-                        </div>
-                        <div className="content" dangerouslySetInnerHTML={htmlDoc}>
-                        </div>
-                    </div>
+                    <Terms userStore={userStore} backButtonAction={backButtonAction} checked={true}/>
                 </MetaData>
             </FadeAnimation>
         );
