b6392e8da7d8cee4f749713305d91ebb7e2ca51e
Jerry Trieu <jerry@advancedtelematic.com>
2018-05-24 14:18:04 +0200

    Add HERE 2018-04/terms-and-conditions

--- ota-plus-web/app/assets/contracts/index.js
+++ ota-plus-web/app/assets/contracts/index.js
@@ -1,7 +1,7 @@
-import terms_conditions from './tc/2018-01/en/terms_conditions.html';
+import terms_conditions from './tc/2018-04/en/terms-and-conditions.html';
 
-export const defaultName = 'tc/2018-01/en/terms_conditions.html';
+export const defaultName = 'tc/2018-04/en/terms-and-conditions.html';
 
 export default {
-    'tc/2018-01/en/terms_conditions.html': terms_conditions
+    'tc/2018-04/en/terms-and-conditions.html': terms_conditions
 }
