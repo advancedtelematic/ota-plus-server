cec212965c1a782e5bbfecfee08e776eca77222b
vhudyma <vhudyma@alliedcoders.com>
2018-05-25 12:37:08 +0300

    Created dashboard page in devices view

--- ota-plus-web/app/reactapp/src/components/devices/index.js
+++ ota-plus-web/app/reactapp/src/components/devices/index.js
@@ -1,7 +1,11 @@
 import DevicesGroupsPanel from './GroupsPanel';
 import DevicesContentPanel from './ContentPanel';
+import DevicesBarChart from './BarChart';
+import DevicesStats from './Stats';
 
 export {
     DevicesGroupsPanel,
-    DevicesContentPanel
+    DevicesContentPanel,
+    DevicesBarChart,
+    DevicesStats
 }
