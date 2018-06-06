e736f78e78308e6ad25564f7949284f826095eed
koshelev <vladimir.koshelev@here.com>
2018-06-06 15:56:37 +0200

    Make sign-up url configurable

--- ota-plus-web/app/com/advancedtelematic/auth/garage/Auth0Config.scala
+++ ota-plus-web/app/com/advancedtelematic/auth/garage/Auth0Config.scala
@@ -4,13 +4,15 @@ import play.api.Configuration
 
 final case class Auth0Config(
     audience: String,
-    dbConnection: String
+    dbConnection: String,
+    signUpUrl: String
 )
 
 object Auth0Config {
   def apply(configuration: Configuration): Auth0Config = {
     val audience = configuration.get[String]("auth0.audience")
     val dbConnection = configuration.get[String]("auth0.dbConnection")
-    Auth0Config(audience, dbConnection)
+    val signUpUrl = configuration.get[String]("auth0.signUpUrl")
+    Auth0Config(audience, dbConnection, signUpUrl)
   }
 }
