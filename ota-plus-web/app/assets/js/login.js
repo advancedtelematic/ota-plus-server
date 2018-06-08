0046a69a2bc42b992a25fb1d923b1a8ae278fe65
vhudyma <vhudyma@alliedcoders.com>
2018-06-08 15:32:17 +0300

    Restyling auth0 page

--- ota-plus-web/app/assets/js/login.js
+++ ota-plus-web/app/assets/js/login.js
@@ -2,9 +2,10 @@ $(function () {
   $('#ats-page').fadeIn('slow');
 
   var appUrl = location.protocol + '//' + location.host;
-  var logoUrl = appUrl + '/assets/img/ATS-Garage_Logo_white.svg';
+  var logoUrl = appUrl + '/assets/img/HERE_pos.png';
   var options = {
     oidcConformant: true,
+    allowSignUp: false,
     theme: {
       logo: logoUrl
     },
@@ -18,16 +19,14 @@ $(function () {
     },
     container: 'widget-container',
     languageDictionary: {
-      title: "Login to ATS Garage.",
-      signupTitle: "Register to ATS Garage.",
-      loginLabel: "Already registered? Login here.",
-      loginSubmitLabel: "Login",
+      title: "Get more from HERE",
+      loginSubmitLabel: "Sign in",
+      socialLoginInstructions: "ATS Garage is now HERE OTA Connect...",
+      databaseEnterpriseAlternativeLoginInstructions: "Sign in to your HERE account",
+      forgotPasswordAction: "Forgot your password?",
       forgotPasswordTitle: 'Password reset',
-      forgotPasswordAction: "Forgot Password >",
       forgotPasswordInstructions: "Please enter your email address. We will send you a link to reset your password.",
       forgotPasswordSubmitLabel: "Send",
-      signUpLabel: 'Not registered yet? Sign up here.',
-      signUpSubmitLabel: 'Start now!'
     }
   }
 
@@ -37,44 +36,12 @@ $(function () {
     alert(err);
   });
 
-  lock.show();
+ lock.show(options);
 
- lock.on('signin ready', function () {
-   $('.auth0-lock').addClass('signin');
-   $('.auth0-lock').removeClass('signup');
-
-   $('.auth0-lock-name').text(options.languageDictionary.title);
-
-   $('input.auth0-lock-input[name="email"]').attr('id','a0-signin_easy_email');
-   $('input.auth0-lock-input[name="password"]').attr('id','a0-signin_easy_password');
-   $('.auth0-lock-form .auth0-lock-tabs-current + li > a').attr('id','login-signup-switch');
-   $('.auth0-lock-alternative > a').attr('id','forgot_password_link');
-   $('.auth0-lock-social-button[data-provider="github"]').attr('id','github_social_link');
-   $('.auth0-lock-social-button[data-provider="google-oauth2"]').attr('id','google_social_link');
-
-   if($('.terms-conditions').length) {
-    $('.terms-conditions').remove();
-   }
+ lock.on('signin ready', function() {
+  var registerLink = "<a href='https://developer.here.com/authenticationpage' class='auth0-register-link'>Register for a HERE account</a>";
+  if(!$('.auth0-register-link').length) {
+    $('.auth0-lock-header-welcome').append(registerLink);
+  }
  });
- lock.on('signup ready', function () {
-   $('.auth0-lock').removeClass('signin');
-   $('.auth0-lock').addClass('signup');
-
-   $('.auth0-lock-name').text(options.languageDictionary.signupTitle);
-
-   $('input.auth0-lock-input[name="email"]').attr('id','a0-signup_easy_email');
-   $('input.auth0-lock-input[name="password"]').attr('id','a0-signup_easy_password');
-   $('.auth0-lock-form .auth0-lock-tabs li:not(.auth0-lock-tabs-current) > a').attr('id','login-signup-switch');
-   $('.auth0-lock-social-button[data-provider="github"]').attr('id','github_social_link');
-   $('.auth0-lock-social-button[data-provider="google-oauth2"]').attr('id','google_social_link');
-   
-   var link = $("<div class='terms-conditions'>By registering I agree to <a href='http://atsgarage.com/en/terms-conditions.html' target='_blank'>ATS Garage's Terms of Use.</a></div>");
-   if(!$('.terms-conditions').length)
-     link.appendTo('.auth0-lock-center');
- });
-  lock.on('forgot_password ready', function () {
-
-    $('input.auth0-lock-input[name="email"]').attr('id','a0-reset_easy_email');
-
-  });
 });
