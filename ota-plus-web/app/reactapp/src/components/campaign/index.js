06358e74d03792f342360c27ea157125876f50ff
vhudyma <vhudyma@alliedcoders.com>
2018-08-20 09:14:08 +0300

    Created example installation report view chart

--- ota-plus-web/app/reactapp/src/components/campaign/index.js
+++ ota-plus-web/app/reactapp/src/components/campaign/index.js
@@ -2,10 +2,12 @@ import CampaignCancelCampaignModal from './CancelCampaignModal';
 import CampaignCancelGroupModal from './CancelGroupModal';
 import CampaignGroupsList from './GroupsList';
 import CampaignSubHeader from './SubHeader';
+import CampaignInstallationReportView from './InstallationReportView';
 
 export {
     CampaignCancelCampaignModal,
     CampaignCancelGroupModal,
     CampaignGroupsList,
     CampaignSubHeader,
+    CampaignInstallationReportView,
 }
