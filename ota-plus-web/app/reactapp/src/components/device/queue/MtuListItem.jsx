ba9b7386c22ca109439eae52f7fc6aca67b004ff
vhudyma <vhudyma@alliedcoders.com>
2018-08-28 11:33:06 +0300

    Displayed hardware id in the queued MTU item

--- ota-plus-web/app/reactapp/src/components/device/queue/MtuListItem.jsx
+++ ota-plus-web/app/reactapp/src/components/device/queue/MtuListItem.jsx
@@ -1,14 +1,28 @@
 import React, { Component, PropTypes } from 'react';
 import { observer, inject } from 'mobx-react';
 import InstallationEvents from '../InstallationEvents';
+import _ from 'underscore';
 
 @inject("stores")
 @observer
 class MtuListItem extends Component {
     render() {
-        const { item, hardwareId, updateId, status, length, cancelMtuUpdate, showSequencer } = this.props;
-        const { featuresStore } = this.props.stores;
+        const { item, serial, updateId, status, length, cancelMtuUpdate, showSequencer } = this.props;
+        const { featuresStore, devicesStore } = this.props.stores;
         const { alphaPlusEnabled } = featuresStore;
+        const { device } = devicesStore;
+        const devicePrimaryEcu = device.directorAttributes.primary;
+        const deviceSecondaryEcus = device.directorAttributes.secondary;
+
+        let hardwareId = null;
+        if(devicePrimaryEcu.id === serial) {
+            hardwareId = devicePrimaryEcu.hardwareId;
+        }
+        const serialFromSecondary = _.find(deviceSecondaryEcus, ecu => ecu.id === serial)
+        if(serialFromSecondary) {
+            hardwareId = serialFromSecondary.hardwareId;
+        }
+
         const hash = item.image.fileinfo.hashes.sha256;
         return (
             <li id={"queued-entry-" + hash} className="queue-modal__item">
@@ -49,6 +63,14 @@ class MtuListItem extends Component {
                             <div className="queue-modal__operation-info-block">
                                 <span id={"ecu-serial-title-" + updateId} className="queue-modal__operation-info-title">
                                     ECU Serial:
+                                </span>
+                                <span id={"ecu-serial-" + updateId}>
+                                    {serial}
+                                </span>
+                            </div>
+                            <div className="queue-modal__operation-info-block">
+                                <span id={"ecu-serial-title-" + updateId} className="queue-modal__operation-info-title">
+                                    Hardware id:
                                 </span>
                                 <span id={"ecu-serial-" + updateId}>
                                     {hardwareId}
