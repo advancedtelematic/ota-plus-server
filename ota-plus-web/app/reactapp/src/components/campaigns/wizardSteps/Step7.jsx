ce9387c4d5bb6ce6ddab40153afbefb7d5b10725
Binh Hoang <son.binh.hoang@meelogic.com>
2018-08-30 18:35:35 +0200

    [PRO-5532] Fix launch campaign and campaign's data summary and complete update's details

--- ota-plus-web/app/reactapp/src/components/campaigns/wizardSteps/Step7.jsx
+++ ota-plus-web/app/reactapp/src/components/campaigns/wizardSteps/Step7.jsx
@@ -4,106 +4,107 @@ import { observer, inject } from 'mobx-react';
 import { Loader, AsyncResponse } from '../../../partials';
 import { translate } from 'react-i18next';
 import _ from 'underscore';
-import moment from 'moment';
 
 @inject('stores')
 @observer
 class WizardStep7 extends Component {
+    componentWillMount() {
+        const { updatesStore } = this.props.stores;
+        const { wizardData } = this.props;
+        const currentUpdate = _.first(wizardData[2].update);
+        // toDo: to check if mtu data with current source id already is in store before fetch
+        updatesStore.fetchUpdate(currentUpdate && currentUpdate.source.id);
+    }
+
     render() {
         const { t, wizardData } = this.props;
-        const {campaignsStore, groupsStore} = this.props.stores;
-        const updates = wizardData[2].versions;
+        const { campaignsStore, groupsStore, updatesStore } = this.props.stores;
+
+        const updateSummary = updatesStore.currentMtuData;
+
         return (
             <div className="step-inner">
-                <AsyncResponse 
+                <AsyncResponse
                     handledStatus="error"
-                    action={campaignsStore.campaignsCreateAsync}
-                    errorMsg={(campaignsStore.campaignsCreateAsync.data ? campaignsStore.campaignsCreateAsync.data.description : null)}
+                    action={ campaignsStore.campaignsCreateAsync }
+                    errorMsg={ (campaignsStore.campaignsCreateAsync.data ? campaignsStore.campaignsCreateAsync.data.description : null) }
                 />
                 <div className="box-summary">
-                    <div className="title">
-                        Software & Version
-                    </div>
+                    <div className="title">{ "Software & Version" }</div>
                     <div className="desc">
-                        {_.map(updates, (update, packName) => {
+                        {   updateSummary &&
+                            _.map(updateSummary, (target, hardwareId) => {
+                            const noInformation = "No information.";
+                            const fromPackage = target.from.target;
+                            const toPackage = target.to.target;
+                            const fromVersion = target.from.checksum.hash;
+                            const toVersion = target.to.checksum.hash;
+
                             return (
-                                <div className="package-container" key={packName}>
+                                <div className="package-container" key={ hardwareId }>
+                                    <label className="c-form__label">{ hardwareId }</label>
                                     <div className="update-container">
                                         <span className="director-updates">
                                             <div className="update-from">
-                                                <div className="text">
-                                                    From:
-                                                </div>
-                                                <div className="value" id={"from-package-name-" + (!_.isEmpty(update.changedPackage) ? update.changedPackage.packageName : update.toPackageName)}>
-                                                    {!_.isEmpty(update.changedPackage) ?
-                                                        update.changedPackage.packageName
-                                                    :
-                                                        update.toPackageName
-                                                    }
+                                                <div className="text">{ "From:" }</div>
+                                                <div className="value"
+                                                     id={ "from-package-name-" + fromPackage }>
+                                                    { fromPackage ? fromPackage : noInformation }
                                                 </div>
                                             </div>
                                             <div className="update-to">
-                                                <div className="text">
-                                                    To:
-                                                </div>
-                                                <div className="value" id={"to-package-name-" + update.toPackageName}>
-                                                    {update.toPackageName}
+                                                <div className="text">{ "To:" }</div>
+                                                <div className="value" id={ "to-package-name-" + toPackage }>
+                                                    { toPackage ? toPackage : noInformation }
                                                 </div>
                                             </div>
                                         </span>
                                         <span className="director-versions">
                                             <div className="update-from">
-                                                <div className="text">
-                                                    Version:
-                                                </div>
-                                                <div className="value" id={"from-package-version-" + update.from}>
-                                                    {update.from}
+                                                <div className="text">{ "Version:" }</div>
+                                                <div className="value" id={ "from-package-version-" + fromVersion }>
+                                                    { fromVersion ? fromVersion : noInformation }
                                                 </div>
                                             </div>
                                             <div className="update-to">
-                                                <div className="text">
-                                                    Version:
-                                                </div>
-                                                <div className="value" id={"to-package-version-" + update.to}>
-                                                    {update.to}
+                                                <div className="text">{ "Version:" }</div>
+                                                <div className="value" id={ "to-package-version-" + toVersion }>
+                                                    { toVersion ? toVersion : noInformation }
                                                 </div>
                                             </div>
                                         </span>
                                     </div>
                                     <div className="hardware-id-container">
-                                        <div className="text">
-                                            On:
-                                        </div>
-                                        <div className="value app-label" id={"package-version-" + update.to + '-hardware-id'}>
-                                            {update.hardwareId}
+                                        <div className="text">{ "On:" }</div>
+                                        <div className="value app-label"
+                                             id={ "package-version-" + hardwareId }>
+                                            { target.targetFormat }
                                         </div>
                                     </div>
                                 </div>
                             );
-                        })}
+                        }) }
                     </div>
                 </div>
                 <div className="box-summary groups">
-                    <div className="title">
-                        Groups & Devices
-                    </div>
+                    <div className="title">{ "Groups & Devices" }</div>
                     <div className="desc">
                         <div className="wrapper-groups">
-                            {_.map(wizardData[3].groups, (group, index) => {
+                            { _.map(wizardData[1].groups, (group, index) => {
                                 return (
-                                    <div className="element-box group" key={index}>
-                                        <div className="icon"></div>
+                                    <div className="element-box group" key={ index }>
+                                        <div className="icon" />
                                         <div className="desc">
                                             <div className="title" id="wizard-summary-group-name">
-                                                {group.groupName}
+                                                { group.groupName }
                                             </div>
                                             <div className="subtitle" id="wizard-summary-group-devices">
-                                                {t('common.deviceWithCount', {count: groupsStore._getGroupDevices(group).length})}
+                                                { t('common.deviceWithCount', { count: groupsStore._getGroupDevices(group).length }) }
                                             </div>
                                         </div>
                                     </div>
                                 );
-                            })}
+                            }) }
                         </div>
                     </div>
                 </div>
@@ -114,7 +115,7 @@ class WizardStep7 extends Component {
 
 WizardStep7.propTypes = {
     wizardData: PropTypes.object.isRequired,
-    stores: PropTypes.object
-}
+    stores: PropTypes.object,
+};
 
 export default translate()(WizardStep7);
