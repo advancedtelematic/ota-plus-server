8b5b1c4133c208508cdf3115580aa059aa896236
Binh Hoang <son.binh.hoang@meelogic.com>
2018-09-11 14:00:48 +0200

    [OTA-846] fix styling

--- ota-plus-web/app/reactapp/src/components/updates/createWizard/Step1.jsx
+++ ota-plus-web/app/reactapp/src/components/updates/createWizard/Step1.jsx
@@ -61,7 +61,7 @@ class Step1 extends Component {
 
                     <div className="row hardware-container">
                         <div className="col-xs-12">
-                            <div className="ios-list">
+                            <div className="ids-list">
                                 <label className="c-form__label">{ "Select Hardware ids" }</label>
                                 { hardwareStore.hardwareIdsFetchAsync.isFetching ?
                                     <div className="wrapper-center">
