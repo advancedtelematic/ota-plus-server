fab8be28d20782d611c7cb03603caee3f351094e
Binh Hoang <son.binh.hoang@meelogic.com>
2018-09-17 16:19:37 +0200

    [OTA-888] Refactor data passed to Sequencer

--- ota-plus-web/app/reactapp/src/components/campaigns/wizardSteps/Step6.jsx
+++ ota-plus-web/app/reactapp/src/components/campaigns/wizardSteps/Step6.jsx
@@ -1,21 +1,56 @@
 import React, { Component, PropTypes } from 'react';
-import { observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { Sequencer } from '../../../partials';
+import _ from "underscore";
 
+@inject('stores')
 @observer
 class WizardStep6 extends Component {
+
+    componentWillMount() {
+        const { updatesStore } = this.props.stores;
+        const { wizardData } = this.props;
+        const { update } = wizardData;
+        const { source } = _.first(update);
+        updatesStore.fetchUpdate(source && source.id);
+    }
+
+    prepareVersionData = () => {
+        const { updatesStore } = this.props.stores;
+        const { currentMtuData: mtu } = updatesStore;
+        let versions = [];
+        _.each(mtu.data, (data, hardwareId) => {
+            const { checksum: fromVersion } = data.from;
+            const { checksum: toVersion, target } = data.to;
+            versions.push({
+                hardwareId: hardwareId,
+                name: target,
+                from: fromVersion.hash,
+                to: toVersion.hash,
+                filepath: `${target}-${toVersion.hash}`,
+            });
+        });
+        return versions;
+    };
+
     render() {
-        //const { wizardIdentifier, wizardData } = this.props;
-        {/*<Sequencer
-                wizardIdentifier={wizardIdentifier}
-                data={wizardData.versions}
-                entity={"campaign"}
-                readOnly={false}
-            />*/}
+        const { wizardIdentifier } = this.props;
+        const { campaignsStore } = this.props.stores;
+        let versions = this.prepareVersionData();
+
         return (
-            <div className="wrapper-center">
-                { "Currently no information available." }
-            </div>
+            !!versions.length ?
+                <Sequencer
+                    campaignsStore={ campaignsStore }
+                    wizardIdentifier={ wizardIdentifier }
+                    data={ versions }
+                    entity={ "campaign" }
+                    readOnly={ false }
+                />
+                :
+                <div className="wrapper-center">
+                    { "Live installation progress is not available for this update." }
+                </div>
         );
     }
 }
