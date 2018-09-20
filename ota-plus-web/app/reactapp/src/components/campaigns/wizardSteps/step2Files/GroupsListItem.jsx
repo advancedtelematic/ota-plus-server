35654fdfb17cb1c8e1fba238ca8b57571a81de17
JustynaKK <justyna.kordiaczynska@gmail.com>
2018-09-20 16:19:20 +0200

    [OTA-701] MockUp for the dynamic campaign in the campaign wizard [OTA-1009] Smart group icons in the campaign wizard

--- ota-plus-web/app/reactapp/src/components/campaigns/wizardSteps/step2Files/GroupsListItem.jsx
+++ ota-plus-web/app/reactapp/src/components/campaigns/wizardSteps/step2Files/GroupsListItem.jsx
@@ -1,30 +1,68 @@
 import React, { Component, PropTypes } from 'react';
 import { observer, inject } from 'mobx-react';
 import { translate } from 'react-i18next';
+import { observable } from 'mobx';
+import { VelocityTransitionGroup } from 'velocity-react';
+
 
 @inject("stores")
 @observer
 class GroupsListItem extends Component {
+    @observable automaticCampaign = false;
+
+    toggleAutomaticCampaign = () => {
+        this.automaticCampaign = !this.automaticCampaign;
+    }
     render() {
         const { t, group, setWizardData, isChosen } = this.props;
-        const { groupsStore } = this.props.stores;
+        const { groupsStore, featuresStore } = this.props.stores;
+        const { alphaTestEnabled } = featuresStore;
         return (
-            <button className={"item" + (isChosen ? " selected" : "")} id={"button-group-" + group.groupName} onClick={setWizardData.bind(this, group.id)}>
-                <div className={"btn-checkbox" + (isChosen ? " checked" : "")}>
-                    <i className="fa fa-check" aria-hidden="true"/>
-                </div>
-                <div className="element-box group">
-                    <div className="icon"/>
-                    <div className="desc">
-                        <div className="title">
-                            {group.groupName}
-                        </div>
-                        <div className="subtitle">
-                            {t('common.deviceWithCount', {count: groupsStore._getGroupDevices(group).length})}
+            <div>
+                <div className={"item" + (isChosen ? " selected" : "")} id={"button-group-" + group.groupName}>
+                    <button className={"btn-checkbox" + (isChosen ? " checked" : "")}
+                        onClick={setWizardData.bind(this, group.id)}>
+                        <i className="fa fa-check" aria-hidden="true"
+                        />
+                    </button>
+                    <div className="element-box group"
+                        onClick={setWizardData.bind(this, group.id)}>
+                        {group.groupType === 'static' ? < div className="icon icon--default" /> : <div className="icon icon--smart" />}
+                        <div className="desc">
+                            <div className="title">
+                                {group.groupName}
+                            </div>
+                            <div className="subtitle">
+                                {t('common.deviceWithCount', { count: groupsStore._getGroupDevices(group).length })}
+                            </div>
                         </div>
                     </div>
+                    {alphaTestEnabled &&
+                        (group.groupType === 'dynamic' &&
+                            <div className="automatic-campaign" onClick={this.toggleAutomaticCampaign}>
+                                <div>
+                                    automatic campaign
+                                <div className={"switch" + (this.automaticCampaign ? " switchOn" : "")} ></div>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
-            </button>
+
+                <VelocityTransitionGroup
+                    enter={{
+                        animation: "slideDown",
+                    }}
+                    leave={{
+                        animation: "slideUp",
+                    }}
+                >
+                    {
+                        this.automaticCampaign &&
+                        <div className="automatic-campaign-tip">Automatically publish to new matching devices</div>
+                    }
+                </VelocityTransitionGroup>
+            </div >
         );
     }
 }
