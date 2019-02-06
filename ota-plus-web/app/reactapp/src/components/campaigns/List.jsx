8b0ebcd441b3e7adb1bf31bd0c820b5cd5cb9db9
Binh Hoang <son.binh.hoang@meelogic.com>
2019-02-06 17:20:01 +0100

    [OTA-1690] refactor "latest active campaigns" on dashboard and highlight after redirection
    Signed-off-by: Binh Hoang <son.binh.hoang@meelogic.com>

--- ota-plus-web/app/reactapp/src/components/campaigns/List.jsx
+++ ota-plus-web/app/reactapp/src/components/campaigns/List.jsx
@@ -3,7 +3,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import _ from 'lodash';
 
 import ListItem from './ListItem';
 import { contains } from '../../utils/Helpers';
@@ -15,46 +14,9 @@ class List extends Component {
     stores: PropTypes.object,
     status: PropTypes.string.isRequired,
     expandedCampaigns: PropTypes.array.isRequired,
-    focus: PropTypes.string,
     toggleCampaign: PropTypes.func,
     showCancelCampaignModal: PropTypes.func,
     showDependenciesModal: PropTypes.func,
-  };
-
-  componentDidMount() {
-    const { focus } = this.props;
-    this.highlightCampaign(focus);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { focus: prevFocus } = this.props;
-    const { focus: nextFocus } = nextProps;
-
-    if (prevFocus !== nextFocus) {
-      this.highlightCampaign(nextFocus);
-    }
-  }
-
-  scrollToElement = id => {
-    const wrapperPosition = this.refs.list.getBoundingClientRect();
-    const elementCoords = document.getElementById(`item-${id}`).getBoundingClientRect();
-    const scrollTo = elementCoords.top - wrapperPosition.top + 35;
-    const page = document.querySelector('.campaigns');
-    setTimeout(() => {
-      page.scrollTop = scrollTo;
-    }, 1000);
-  };
-
-  highlightCampaign = id => {
-    const { stores, toggleCampaign } = this.props;
-    const { list } = this.refs;
-    const { campaignsStore } = stores;
-
-    if (list && id) {
-      const name = _.filter(campaignsStore.campaigns, obj => obj.id === id);
-      toggleCampaign(name[0].name);
-      this.scrollToElement(id);
-    }
   };
 
   render() {
@@ -65,28 +27,28 @@ class List extends Component {
 
     const campaignsAvailable = !!campaigns.length;
 
-    return campaignsAvailable ?
+    return campaignsAvailable ? (
       <div className='campaigns__list'>
         {campaigns.map(campaign => {
           const isExpanded = contains(expandedCampaigns, campaign);
           const isCancelable = status === 'launched';
           return (
             <ListItem
-              key={ campaign.id }
-              campaign={ campaign }
-              type={ status }
-              isExpanded={ isExpanded }
-              showCancelCampaignModal={ showCancelCampaignModal }
-              showDependenciesModal={ showDependenciesModal }
-              toggleCampaign={ toggleCampaign }
-              isCancelable={ isCancelable }
+              key={campaign.id}
+              campaign={campaign}
+              type={status}
+              isExpanded={isExpanded}
+              showCancelCampaignModal={showCancelCampaignModal}
+              showDependenciesModal={showDependenciesModal}
+              toggleCampaign={toggleCampaign}
+              isCancelable={isCancelable}
             />
           );
         })}
       </div>
-      :
+    ) : (
       <div className='campaigns__list--empty'>{`Currently there are no ${status} campaigns.`}</div>
-    ;
+    );
   }
 }
 
