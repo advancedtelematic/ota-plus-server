96da062b7f6dc44c60c6ad925926e6c382e60b0d
Binh Hoang <son.binh.hoang@meelogic.com>
2019-02-11 15:55:54 +0100

    fix broken create links in dashboard's Dropdown's
    Signed-off-by: Binh Hoang <son.binh.hoang@meelogic.com>

--- ota-plus-web/app/reactapp/src/layouts/Main.jsx
+++ ota-plus-web/app/reactapp/src/layouts/Main.jsx
@@ -119,8 +119,6 @@ class Main extends Component {
         minimizedWizards={this.minimizedWizards}
         skipStep={skipStep}
         key={this.wizards.length}
-        activeTab={this.getActiveTab()}
-        switchTab={this.switchTab}
       />
     );
     this.wizards = this.wizards.concat(wizard);
