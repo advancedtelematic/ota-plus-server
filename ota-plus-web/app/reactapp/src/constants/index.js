a9d6f7e457256377dadad836083f8b8476e8039d
Piotr Smol <piotrsmol.pvt@gmail.com>
2019-03-08 11:02:20 +0100

    [OTA-2233] retry campaign support
    Signed-off-by: Piotr Smol <piotrsmol.pvt@gmail.com>

---
+++ ota-plus-web/app/reactapp/src/constants/index.js
@@ -0,0 +1,14 @@
+/*
+ *  Retry campaign
+ */
+
+export const CAMPAIGN_RETRY_STATUS_TOOLTIPS = {
+    not_launched: 'You can retry the update installation',
+    launched: 'Retrying the update installation',
+    finished: 'Retry cycle finished'
+};
+export const CAMPAIGN_RETRY_STATUSES = {
+    NOT_LAUNCHED: 'not_launched',
+    LAUNCHED: 'launched',
+    FINISHED: 'finished'
+};
