e60131cfc0118e6468ad11b6a0aa8cc6c7bf94e1
Piotr Smol <piotrsmol.pvt@gmail.com>
2019-04-11 16:37:09 +0200

    [OTA-1530] Verify displaying correct data
    Signed-off-by: Piotr Smol <piotrsmol.pvt@gmail.com>

--- ota-plus-web/app/reactapp/src/components/updates/UpdateDetails.jsx
+++ ota-plus-web/app/reactapp/src/components/updates/UpdateDetails.jsx
@@ -3,6 +3,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import { observable } from 'mobx';
 import { Row, Col } from 'antd';
 import { FormInput, FormTextarea, Loader } from '../../partials/index';
 import _ from 'lodash';
@@ -10,10 +11,33 @@ import _ from 'lodash';
 @inject('stores')
 @observer
 class UpdateDetails extends Component {
+  @observable fromVersion = '';
+  @observable toVersion = '';
+
   componentWillMount() {
     const { updatesStore } = this.props.stores;
     const { updateItem } = this.props;
     updatesStore.fetchUpdate(updateItem && updateItem.source && updateItem.source.id);
+  }
+
+  componentDidUpdate() {
+    if (!this.fromVersion && !this.toVersion) {
+      const { updatesStore, softwareStore } = this.props.stores;
+      const mtuData = updatesStore && updatesStore.currentMtuData && updatesStore.currentMtuData.data;
+      _.each(mtuData, (target) => {
+        const { target: fromPackage } = target.from;
+        const { target: toPackage } = target.to;
+        _.find(softwareStore.packages, pack => {
+          const { version } = pack.id;
+          if (pack.filepath === fromPackage) {
+            this.fromVersion = version;
+          }
+          if (pack.filepath === toPackage) {
+            this.toVersion = version;
+          }
+        });
+      });
+    }
   }
 
   render() {
@@ -47,9 +71,8 @@ class UpdateDetails extends Component {
           ) : mtuData ? (
             _.map(mtuData, (target, hardwareId) => {
               const noInformation = 'No information.';
-              const { target: fromPackage, checksum: fromVersion } = target.from;
-              const { target: toPackage, checksum: toVersion } = target.to;
-
+              const { target: fromPackage } = target.from;
+              const { target: toPackage } = target.to;
               return (
                 <Col span={24} className='hardware-container' key={hardwareId}>
                   <label className='c-form__label' id={`label-hardware-${hardwareId}`}>
@@ -69,10 +92,10 @@ class UpdateDetails extends Component {
                   </Row>
                   <Row className='row'>
                     <Col span={12}>
-                      <FormInput label='Version' name='fromVersion' id={`${hardwareId}-from-version`} defaultValue={fromVersion ? fromVersion.hash : noInformation} isEditable={isEditable} />
+                      <FormInput label='Version' name='fromVersion' id={`${hardwareId}-from-version`} defaultValue={this.fromVersion || noInformation} isEditable={isEditable} />
                     </Col>
                     <Col span={12}>
-                      <FormInput label='Version' name='toVersion' id={`${hardwareId}-to-version`} defaultValue={toVersion ? toVersion.hash : noInformation} isEditable={isEditable} />
+                      <FormInput label='Version' name='toVersion' id={`${hardwareId}-to-version`} defaultValue={this.toVersion || noInformation} isEditable={isEditable} />
                     </Col>
                   </Row>
                 </Col>
