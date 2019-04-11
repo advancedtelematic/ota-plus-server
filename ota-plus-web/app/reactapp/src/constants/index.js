8e34928795d4dda04a68d664fbba92485f9036fe
Piotr Smol <piotrsmol.pvt@gmail.com>
2019-04-11 12:50:32 +0200

    [OTA-2233] Retry improvements + StatisticsBar fix
    Signed-off-by: Piotr Smol <piotrsmol.pvt@gmail.com>

--- ota-plus-web/app/reactapp/src/constants/index.js
+++ ota-plus-web/app/reactapp/src/constants/index.js
@@ -5,10 +5,12 @@
 export const CAMPAIGN_RETRY_STATUS_TOOLTIPS = {
     not_launched: 'You can retry the update installation',
     launched: 'Retrying the update installation',
+    waiting: 'Please, wait to retry the update installation',
     finished: 'Retry cycle finished'
 };
 export const CAMPAIGN_RETRY_STATUSES = {
     NOT_LAUNCHED: 'not_launched',
     LAUNCHED: 'launched',
+    WAITING: 'waiting',
     FINISHED: 'finished'
 };
