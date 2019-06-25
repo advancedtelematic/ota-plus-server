bc3708150a47921a6059622cf36a96b3267c83e6
Piotr Cichon <ext-piotr.cichon@here.com>
2019-06-25 09:46:18 +0200

    [OTA-3081] Software packages tab-bar mis-aligned on alpha account
    Signed-off-by: Piotr Cichon <ext-piotr.cichon@here.com>

--- ota-plus-web/app/reactapp/src/components/software/Header.jsx
+++ ota-plus-web/app/reactapp/src/components/software/Header.jsx
@@ -2,34 +2,99 @@
 
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import { Button } from 'antd';
+import { inject, observer } from 'mobx-react';
+import { Button, Tag } from 'antd';
+import { action } from 'mobx';
 
+@inject('stores')
 @observer
 class Header extends Component {
+  static propTypes = {
+    stores: PropTypes.shape({}),
+    showCreateModal: PropTypes.func.isRequired,
+    switchToSWRepo: PropTypes.bool.isRequired
+  };
+
+  static defaultProps = {
+    stores: {}
+  };
+
+  componentWillMount() {
+    const { stores } = this.props;
+    const { softwareStore } = stores;
+    this.setActive(softwareStore.activeTab);
+  }
+
+  componentWillUnmount() {
+    this.storeActive(this.activeTab);
+  }
+
+  @action
+  setActive = (tab) => {
+    this.activeTab = tab;
+    this.storeActive(tab);
+  };
+
+  @action
+  storeActive = (tab) => {
+    const { stores } = this.props;
+    const { softwareStore } = stores;
+    softwareStore.activeTab = tab;
+  };
+
+  isActive = (tab) => {
+    return (tab === this.activeTab ? 'tab-navigation__link--active' : '');
+  };
+
   render() {
-    const { showCreateModal } = this.props;
+    const { showCreateModal, stores, switchToSWRepo } = this.props;
+    const { featuresStore } = stores;
+    const { alphaPlusEnabled } = featuresStore;
     return (
       <div className="tab-navigation">
-        <div className="tab-navigation__buttons">
-          <Button
-            htmlType="button"
-            className="ant-btn ant-btn-outlined"
-            id="add-new-software"
-            onClick={() => {
-              showCreateModal(null);
-            }}
-          >
-            {'Add software'}
-          </Button>
-        </div>
+        {alphaPlusEnabled && (
+          <ul className="tab-navigation__links">
+            <li
+              onClick={() => {
+                this.setActive('compact');
+              }}
+              className={`tab-navigation__link ${this.isActive('compact')}`}
+            >
+              <span>{'Compact'}</span>
+            </li>
+            <li
+              onClick={() => {
+                this.setActive('advanced');
+              }}
+              className={`tab-navigation__link ${this.isActive('advanced')}`}
+            >
+              <span>
+                {'Advanced'}
+                <Tag color="#48dad0" className="alpha-tag">
+                  ALPHA
+                </Tag>
+              </span>
+            </li>
+          </ul>
+        )}
+
+        {!switchToSWRepo && (
+          <div className="tab-navigation__buttons">
+            <Button
+              htmlType="button"
+              className="ant-btn ant-btn-outlined"
+              id="add-new-software"
+              onClick={() => {
+                showCreateModal(null);
+              }}
+            >
+              {'Add software'}
+            </Button>
+          </div>
+        )}
       </div>
     );
   }
 }
-
-Header.propTypes = {
-  showCreateModal: PropTypes.func.isRequired,
-};
 
 export default Header;
