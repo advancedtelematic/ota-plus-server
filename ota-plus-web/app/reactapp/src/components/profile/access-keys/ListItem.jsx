ef8a191ed03e93ae71c499a90fca884a272eaf0c
Piotr Smol <piotrsmol.pvt@gmail.com>
2019-06-26 15:46:42 +0200

    [OTA-2123] Handle provisioning download errors
    Signed-off-by: Piotr Smol <piotrsmol.pvt@gmail.com>

--- ota-plus-web/app/reactapp/src/components/profile/access-keys/ListItem.jsx
+++ ota-plus-web/app/reactapp/src/components/profile/access-keys/ListItem.jsx
@@ -2,28 +2,44 @@
 
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 
+@inject('stores')
 @observer
 class ListItem extends Component {
   constructor(props) {
     super(props);
   }
-  _onDownload() {
-    const { provisioningKey } = this.props;
-    location.href = '/api/v1/clienttools/provisioning/' + provisioningKey.id;
+
+  onDownload = () => {
+    const { provisioningKey, stores } = this.props;
+    const { provisioningStore } = stores;
+    provisioningStore.downloadProvisioningKey(provisioningKey.id);
   }
+
   render() {
     const { provisioningKey } = this.props;
     const validFrom = new Date(provisioningKey.validFrom);
     const validUntil = new Date(provisioningKey.validUntil);
     return (
       <div className='box' id={'key-' + provisioningKey.description}>
-        <div className='column'>{provisioningKey.description}</div>
-        <div className='column'>Start date: {validFrom.toDateString() + ' ' + validFrom.toLocaleTimeString()}</div>
-        <div className='column'>End date: {validUntil.toDateString() + ' ' + validUntil.toLocaleTimeString()}</div>
         <div className='column'>
-          <img src='/assets/img/icons/download_key.svg' className='download-key-link' id={'download-key-link-' + provisioningKey.description} alt='Icon' onClick={this._onDownload.bind(this)} />
+          {provisioningKey.description}
+        </div>
+        <div className='column'>
+          {`Start date: ${validFrom.toDateString()}  ${validFrom.toLocaleTimeString()}`}
+        </div>
+        <div className='column'>
+          {`End date: ${validUntil.toDateString()} ${validUntil.toLocaleTimeString()}`}
+        </div>
+        <div className='column'>
+          <img 
+            src='/assets/img/icons/download_key.svg' 
+            className='download-key-link' 
+            id={`download-key-link-${provisioningKey.description}`} 
+            alt='Icon' 
+            onClick={this.onDownload} 
+          />
         </div>
       </div>
     );
