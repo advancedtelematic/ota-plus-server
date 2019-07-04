4fb5cd988ce13cee3a3dba836dfbffbc081d5c0a
Ivan Kliuk <ext-ivan.kliuk@here.com>
2019-07-04 13:38:10 +0300

    [OTA-2957] Add JSON log message formatter
    Signed-off-by: Ivan Kliuk <ext-ivan.kliuk@here.com>

--- ota-plus-web/app/com/advancedtelematic/LoggingFilter.scala
+++ ota-plus-web/app/com/advancedtelematic/LoggingFilter.scala
@@ -1,48 +1,61 @@
 package com.advancedtelematic
 
-import javax.inject.Inject
-
-import com.advancedtelematic.api.RemoteApiError
-import play.api.Logger
-import play.api.mvc.{Filter, RequestHeader, Result}
 import _root_.akka.stream.Materializer
+import akka.actor._
+import com.advancedtelematic.api.RemoteApiError
+import com.advancedtelematic.libats.http.logging.RequestLoggingActor
+import javax.inject.{Inject, Named}
+import play.api.http.Status
+import play.api.libs.json._
+import play.api.mvc.{Filter, RequestHeader, Result}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-class LoggingFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
-  lazy val serviceName = sys.env.getOrElse("SERVICE_NAME", "ota-plus-web")
+class LoggingFilter @Inject()(@Named("logging-actor") loggingActor: ActorRef)
+                             (implicit val mat: Materializer, ec: ExecutionContext)
+    extends Filter {
 
-  def apply(nextFilter: RequestHeader => Future[Result])
-           (requestHeader: RequestHeader): Future[Result] = {
+  import LoggingFilter._
 
-    val startTime = System.currentTimeMillis
+  def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    val logMessage = (getMetrics(requestHeader, System.currentTimeMillis) _).tupled andThen getLogMsg
+
     nextFilter(requestHeader) andThen {
       case Success(result) =>
-        logRequest(requestHeader, result.header.status, startTime)
+        loggingActor ! logMessage((result.header.status, result.body.contentLength.getOrElse(0)))
       case Failure(RemoteApiError(result, _)) =>
-        logRequest(requestHeader, result.header.status, startTime)
+        loggingActor ! logMessage((result.header.status, result.body.contentLength.getOrElse(0)))
       case _ =>
-        logRequest(requestHeader, 500, startTime)
+        loggingActor ! logMessage((Status.INTERNAL_SERVER_ERROR, 0))
     }
   }
+}
 
-  protected def logRequest(requestHeader: RequestHeader, resultCode: Int, startTime: Long): Unit = {
-    val endTime = System.currentTimeMillis
+object LoggingFilter {
+
+  private val getLogMsg: Map[String, String] => RequestLoggingActor.LogMsg =
+    LoggingFilter.format _ andThen ((m: String) => RequestLoggingActor.LogMsg(m, Map.empty))
+
+  val serviceName    = sys.env.getOrElse("SERVICE_NAME", "ota-plus-web")
+
+  def getMetrics(requestHeader: RequestHeader, startTime: Long)
+                (responseCode: Int, responseContentLength: Long): Map[String, String] = {
+    val endTime     = System.currentTimeMillis
     val serviceTime = endTime - startTime
     val traceId = requestHeader.headers.get("x-b3-traceid").map("traceid" -> _).toMap
 
-    val metrics = Map(
-      "service_name" -> serviceName,
-      "method" -> requestHeader.method,
-      "path" -> requestHeader.path,
-      "stime" -> serviceTime.toString,
-      "status" -> resultCode.toString
-    ) ++ traceId
-
-    val msg = metrics.toList.map { case (m, v) => s"$m=$v"}.mkString(" ")
-
-    Logger.info(msg)
+    Map(
+      "http_method" -> requestHeader.method,
+      "http_path" -> requestHeader.path,
+      "http_query" -> s"'${requestHeader.rawQueryString}'",
+      "http_stime" -> serviceTime.toString,
+      "http_status" -> responseCode.toString,
+      "http_service_name" -> serviceName) ++
+      traceId ++
+      (if (responseContentLength > 0) Map("http_content_ln" -> responseContentLength.toString) else Map.empty)
   }
+
+  def format(metrics: Map[String, String]): String = Json.stringify(Json.toJson(metrics))
 
 }
