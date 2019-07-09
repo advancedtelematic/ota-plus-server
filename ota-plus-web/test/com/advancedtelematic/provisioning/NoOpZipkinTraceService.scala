7921638d0e0351a57cb27a953f083d37bde25fc9
Simão Mata <simao.mata@here.com>
2019-07-09 16:36:52 +0200

    Inject NoOp zipkin tracer in tests
    Signed-off-by: Simão Mata <simao.mata@here.com>

--- ota-plus-web/test/com/advancedtelematic/provisioning/NoOpZipkinTraceService.scala
+++ ota-plus-web/test/com/advancedtelematic/provisioning/NoOpZipkinTraceService.scala
@@ -8,10 +8,14 @@ import zipkin2.reporter.Reporter
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext
 
-class TestZipkinTraceService extends ZipkinTraceServiceLike {
+class NoOpZipkinTraceService extends ZipkinTraceServiceLike {
   override implicit val executionContext: ExecutionContext = ExecutionContext.global
   val reporter = new TestReporter()
-  override val tracing: Tracing = Tracing.newBuilder().spanReporter(reporter).build()
+  override val tracing: Tracing = {
+    val t = Tracing.newBuilder().supportsJoin(false).spanReporter(reporter).build()
+    t.setNoop(true)
+    t
+  }
 }
 
 class TestReporter extends Reporter[Span] {
