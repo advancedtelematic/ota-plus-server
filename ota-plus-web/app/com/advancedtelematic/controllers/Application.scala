09c12c2c6c82c0eb428d9fdc5cc98a345d1b3ef4
Ignacio Lucero <ignacio.lucero@here.com>
2019-07-26 10:34:57 +0100

    Change endpoints used to cancel pending device updates
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>

--- ota-plus-web/app/com/advancedtelematic/controllers/Application.scala
+++ ota-plus-web/app/com/advancedtelematic/controllers/Application.scala
@@ -108,6 +108,7 @@ class Application @Inject() (ws: TraceWSClient,
   private val directorProxiedPrefixes: Dispatcher = {
     case (_, "multi_target_updates" :: _) => directorApiUri
     case (_, "admin" :: _) => directorApiUri
+    case (_, "assignments" :: _) => directorApiUri
   }
 
   private val coreProxiedPrefixes: Dispatcher = {
