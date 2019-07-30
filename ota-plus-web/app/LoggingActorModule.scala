e80a7b9ea202314f7b0868be493b9ff22602a31c
Ivan Kliuk <ext-ivan.kliuk@here.com>
2019-07-30 12:42:32 +0300

    [OTA-2957] Add JSON log message formatter (#2021)
    Signed-off-by: Ivan Kliuk <ext-ivan.kliuk@here.com>

---
+++ ota-plus-web/app/LoggingActorModule.scala
@@ -0,0 +1,11 @@
+package com.advancedtelematic
+
+import akka.event.Logging
+import com.advancedtelematic.libats.http.logging.RequestLoggingActor
+import com.google.inject.{AbstractModule, Provider, Provides}
+import play.api.libs.concurrent.AkkaGuiceSupport
+
+class LoggingActorModule extends AbstractModule with AkkaGuiceSupport {
+  override def configure(): Unit =
+    bindActor[RequestLoggingActor]("logging-actor", _ => RequestLoggingActor.props(Logging.InfoLevel))
+}
