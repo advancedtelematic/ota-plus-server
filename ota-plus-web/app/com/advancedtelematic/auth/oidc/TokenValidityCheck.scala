15abe4591f4be092352a54c14f434066c06b73fe
Ignacio Lucero <ignacio.lucero@here.com>
2019-08-27 17:23:47 +0200

    Stop validating the expiration time of the session access token.
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>

--- ota-plus-web/app/com/advancedtelematic/auth/oidc/TokenValidityCheck.scala
+++ ota-plus-web/app/com/advancedtelematic/auth/oidc/TokenValidityCheck.scala
@@ -1,13 +1,11 @@
 package com.advancedtelematic.auth.oidc
 
-import java.time.Instant
-
 import com.advancedtelematic.auth.{AccessToken, TokenVerification}
 
 import scala.concurrent.Future
 
 class TokenValidityCheck extends TokenVerification {
   override def apply(token: AccessToken): Future[Boolean] = {
-    Future.successful(Instant.now().isBefore(token.expiresAt))
+    Future.successful(true)
   }
 }
