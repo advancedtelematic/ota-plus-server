05ef9444a6b1ea437c04756eea920a6cdd26be40
Merlin Carter <merlin.carter@here.com>
2019-09-03 16:25:38 +0200

    fixing doc URLs
    Adding upated locations for outdated doc URLs
    
    Signed-off-by: Merlin Carter <merlin.carter@here.com>

--- ota-plus-web/app/reactapp/src/constants/urlConstants.js
+++ ota-plus-web/app/reactapp/src/constants/urlConstants.js
@@ -1,5 +1,5 @@
-export const URL_AKTUALIZR_CONFIG_OPTIONS = 'https://docs.ota.here.com/client-config/aktualizr-config-options.html';
-export const URL_CONFIGURING_DEVICE = 'https://github.com/advancedtelematic/aktualizr/blob/master/docs/configuration.adoc';
-export const URL_GET_STARTED = 'https://docs.ota.here.com/getstarted/dev/start-intro.html';
-export const URL_QUICKSTART = 'https://docs.ota.here.com/quickstarts/start-intro.html';
-export const URL_SOFTWARE_PUSHING_UPDATES = 'https://docs.ota.here.com/quickstarts/pushing-updates.html';
+export const URL_AKTUALIZR_CONFIG_OPTIONS = 'https://docs.ota.here.com/ota-client/dev/aktualizr-config-options.html';
+export const URL_CONFIGURING_DEVICE = 'https://docs.ota.here.com/ota-client/dev/aktualizr-config-options.html';
+export const URL_GET_STARTED = 'https://docs.ota.here.com/getstarted/dev/get-started.html';
+export const URL_QUICKSTART = 'https://docs.ota.here.com/getstarted/dev/get-started.html';
+export const URL_SOFTWARE_PUSHING_UPDATES = 'https://docs.ota.here.com/ota-client/dev/pushing-updates.html';
