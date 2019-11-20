af2bb630bf3e6bd4f6049f65c2f260dcfcd9c2e0
Piotr Smol <piotrsmol.pvt@gmail.com>
2019-11-20 11:04:50 +0100

    [OTA-3931] Help Sidebar rework
    Signed-off-by: Piotr Smol <piotrsmol.pvt@gmail.com>

--- ota-plus-web/app/reactapp/src/constants/urlConstants.js
+++ ota-plus-web/app/reactapp/src/constants/urlConstants.js
@@ -1,5 +1,7 @@
+export const MAIL_SUPPORT = 'mailto:otaconnect.support@here.com';
 export const URL_AKTUALIZR_CONFIG_OPTIONS = 'https://docs.ota.here.com/ota-client/dev/aktualizr-config-options.html';
 export const URL_CONFIGURING_DEVICE = 'https://docs.ota.here.com/ota-client/dev/aktualizr-config-options.html';
+export const URL_DOCS = 'https://docs.ota.here.com/getstarted/dev/index.html';
 export const URL_GET_STARTED = 'https://docs.ota.here.com/getstarted/dev/get-started.html';
 export const URL_QUICKSTART = 'https://docs.ota.here.com/getstarted/dev/get-started.html';
 export const URL_SOFTWARE_PUSHING_UPDATES = 'https://docs.ota.here.com/ota-client/dev/pushing-updates.html';
---
+++ ota-plus-web/app/reactapp/src/partials/HelpSidebar/__tests__/HelpSidebar.spec.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../theme';
+import stores from '../../../stores';
+import HelpSidebar from '..';
+
+function mountComponent(props) {
+  return mount(
+    <MemoryRouter>
+      <Provider stores={stores}>
+        <ThemeProvider theme={theme}>
+          <HelpSidebar {...props} />
+        </ThemeProvider>
+      </Provider>
+    </MemoryRouter>
+  );
+}
+
+describe('<HelpSidebar />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mountComponent();
+  });
+
+  it('renders correctly', () => {
+    expect(wrapper.exists('#help-icon')).toEqual(true);
+  });
+
+  it('should display Help Sidebar when help icon is clicked', () => {
+    expect(wrapper.exists('.ant-drawer-open')).toEqual(false);
+    wrapper.find('#help-icon').first().simulate('click');
+    expect(wrapper.exists('.ant-drawer-open')).toEqual(true);
+  });
+
+  it('should close Help Sidebar when user clicks elsewhere', () => {
+    wrapper.find('#help-icon').first().simulate('click');
+    expect(wrapper.exists('.ant-drawer-open')).toEqual(true);
+    // Sidebar should close when user clicks anywhere on sidebar mask (navbar buttons are also affected)
+    wrapper.find('.ant-drawer-mask').first().simulate('click');
+    expect(wrapper.exists('.ant-drawer-open')).toEqual(false);
+  });
+
+  it('should display 3 links', () => {
+    const linksCount = 3;
+    wrapper.find('#help-icon').first().simulate('click');
+    expect(wrapper.find('a').length).toBe(linksCount);
+  });
+
+  it('should open each link in new tab', () => {
+    wrapper.find('#help-icon').first().simulate('click');
+    wrapper.find('a').forEach((node) => {
+      expect(node.props().target).toEqual('_blank');
+    });
+  });
+});
