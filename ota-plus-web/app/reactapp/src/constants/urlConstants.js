22df7c6cad4214a29badd9eadf1a421a10a09e45
Piotr Smol <piotrsmol.pvt@gmail.com>
2019-11-22 11:32:55 +0100

    [OTA-3928 & OTA-3930 & OTA-3931] Izumi and Omniture integration
    Signed-off-by: Piotr Smol <piotrsmol.pvt@gmail.com>

--- ota-plus-web/app/reactapp/src/constants/urlConstants.js
+++ ota-plus-web/app/reactapp/src/constants/urlConstants.js
@@ -1,7 +1,8 @@
 export const MAIL_SUPPORT = 'mailto:otaconnect.support@here.com';
 export const URL_AKTUALIZR_CONFIG_OPTIONS = 'https://docs.ota.here.com/ota-client/dev/aktualizr-config-options.html';
 export const URL_CONFIGURING_DEVICE = 'https://docs.ota.here.com/ota-client/dev/aktualizr-config-options.html';
-export const URL_DOCS = 'https://docs.ota.here.com/getstarted/dev/index.html';
-export const URL_GET_STARTED = 'https://docs.ota.here.com/getstarted/dev/get-started.html';
+export const URL_DEV_GUIDE = 'https://docs.ota.here.com/ota-client/latest/index.html';
+export const URL_GET_STARTED = 'https://docs.ota.here.com/getstarted/dev/index.html';
 export const URL_QUICKSTART = 'https://docs.ota.here.com/getstarted/dev/get-started.html';
 export const URL_SOFTWARE_PUSHING_UPDATES = 'https://docs.ota.here.com/ota-client/dev/pushing-updates.html';
+export const URL_USER_GUIDE = 'https://docs.ota.here.com/ota-web/dev/index.html';
