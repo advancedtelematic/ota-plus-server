e28450271e00fd193c7eea4f3363391c5bf4c718
Piotr Cichon <ext-piotr.cichon@here.com>
2020-02-26 11:22:10 +0100

    Bugfix - sidebar depends on features.
    Signed-off-by: Piotr Cichon <ext-piotr.cichon@here.com>

--- ota-plus-web/app/reactapp/src/partials/Sidebar/index.jsx
+++ ota-plus-web/app/reactapp/src/partials/Sidebar/index.jsx
@@ -2,36 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Drawer } from 'antd';
 import { SIZES } from '../../constants/styleConstants';
+import { FEATURES } from '../../config';
 
-const NAVBARS_HEIGHT = SIZES.NAVBAR_HEIGHT + SIZES.SUBNAVBAR_HEIGHT;
+const Sidebar = ({ children, features, onClose, visible }) => {
+  const NAVBARS_HEIGHT = features.includes(FEATURES.ORGANIZATIONS)
+    ? SIZES.NAVBAR_HEIGHT + SIZES.SUBNAVBAR_HEIGHT : SIZES.NAVBAR_HEIGHT;
 
-const Sidebar = ({ children, onClose, visible }) => (
-  <Drawer
-    width={`${SIZES.SIDEBAR_WIDTH}px`}
-    maskStyle={{
-      backgroundColor: 'transparent',
-      top: `-${NAVBARS_HEIGHT}px`,
-    }}
-    style={{
-      top: `${NAVBARS_HEIGHT}px`,
-      maxHeight: `calc(100% - ${NAVBARS_HEIGHT}px)`
-    }}
-    bodyStyle={{
-      padding: 0
-    }}
-    closable={false}
-    onClose={onClose}
-    visible={visible}
-  >
-    {children}
-  </Drawer>
-);
+  return (
+    <Drawer
+      width={`${SIZES.SIDEBAR_WIDTH}px`}
+      maskStyle={{
+        backgroundColor: 'transparent',
+        top: `-${NAVBARS_HEIGHT}px`,
+      }}
+      style={{
+        top: `${NAVBARS_HEIGHT}px`,
+        maxHeight: `calc(100% - ${NAVBARS_HEIGHT}px)`
+      }}
+      bodyStyle={{
+        padding: 0
+      }}
+      closable={false}
+      onClose={onClose}
+      visible={visible}
+    >
+      {children}
+    </Drawer>
+  );
+};
 
 Sidebar.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
+  features: PropTypes.arrayOf(PropTypes.string),
   onClose: PropTypes.func.isRequired,
   visible: PropTypes.bool.isRequired
 };
